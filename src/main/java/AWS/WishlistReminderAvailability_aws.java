1aedf3a37d47567263fd10732b78a360df2fcecc
Alexei <alexeym@pkwteile.de>
2019-10-31 16:46:43 +0200

    change pom for allure integration

--- src/main/java/AWS/WishlistReminderAvailability_aws.java
+++ src/main/java/AWS/WishlistReminderAvailability_aws.java
@@ -13,7 +13,7 @@ public class WishlistReminderAvailability_aws {
   private String currentDate = new SimpleDateFormat("yyyy/MM/dd").format(new Date()).replace("/", "-");
   public String urlWithParameters = "https://aws.autodoc.de/wishlist/reminder/availability?Filter%5Bproject%5D%5B0%5D=_" + "DE"
           + "&Filter%5Bdate%5D%5BdateFrom%5D=" + currentDate + "&Filter%5Bdate%5D%5BdateTo%5D=" + currentDate
-          + "&submit=%D0%9F%D0%BE%D0%B8%D1%81%D0%BA"; // todo будем ли выносить в базу ?
+          + "&submit=%D0%9F%D0%BE%D0%B8%D1%81%D0%BA"; // TODO будем ли выносить в базу ?
 
   public SelenideElement articleOfFirstProduct() {
     return $(byXpath(".//*[@id='availability_reminder_table']//tr[1]/td[3]"));
