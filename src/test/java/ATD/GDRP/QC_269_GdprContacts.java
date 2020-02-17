cea517ed7dec7e786dee8a7a19bf9f61f50165ae
Alexei <alexeym@pkwteile.de>
2020-02-17 18:29:54 +0200

    new qc_269 Order aws null condition fix

---
+++ src/test/java/ATD/GDRP/QC_269_GdprContacts.java
@@ -0,0 +1,57 @@
+package ATD.GDRP;
+
+import ATD.Contact_static_page_Logic;
+import ATD.SetUp;
+import AWS.PrivacyPolicySubscription_aws;
+import io.qameta.allure.Description;
+import io.qameta.allure.Flaky;
+import io.qameta.allure.Owner;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.sql.SQLException;
+
+import static ATD.CommonMethods.openPage;
+import static ATD.SetUp.setUpBrowser;
+
+public class QC_269_GdprContacts {
+
+
+    private Contact_static_page_Logic contact_static_page_logic = new Contact_static_page_Logic();
+    private String mail;
+
+    @BeforeClass
+    void setUp() {
+        setUpBrowser(false, "chrome", "77.0");
+    }
+
+    @DataProvider(name = "route")
+    Object[] dataProvider() throws SQLException {
+        return new SetUp().setUpShopWithSubroutes("prod", "DE", "main", "staticContact");
+    }
+
+    @Test(dataProvider = "route", enabled = false)
+    @Flaky
+    @Owner(value = "alex_qa")
+    @Description(value = "Test verify working GDPR form on noOrder tab")
+    public void testGdprNoOrderTab(String route) {
+        openPage(route);
+        mail = contact_static_page_logic.checkingDatenschutzerklarungLinkBehavior(contact_static_page_logic.noOrderDatenschutzerklarungLink())
+                .fillRequiredFieldsNoOrderAndCheckBehavior("qc_269_");
+        new PrivacyPolicySubscription_aws().openPolicySubscriptionWithLogin().checkingPolicyForMail(this.mail);
+    }
+
+    @Test(dataProvider = "route", enabled = true)
+    @Flaky
+    @Owner(value = "alex_qa")
+    @Description(value = "Test verify working GDPR form on Order tab")
+    public void testGdprOrderTab(String route) {
+        openPage(route);
+        mail = contact_static_page_logic.switchToOrderTab().checkingDatenschutzerklarungLinkBehavior(contact_static_page_logic.orderDatenschutzerklarungLink())
+                .fillRequiredFieldsOrderAndCheckBehavior("qc_269_");
+        new PrivacyPolicySubscription_aws().openPolicySubscriptionWithLogin().checkingPolicyForMail(this.mail);
+    }
+}
+
+
