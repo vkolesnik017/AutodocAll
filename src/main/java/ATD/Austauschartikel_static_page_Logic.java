fb74c70fc587cdd76c72cc8d954aa904420c64e3
Alexei <alexeym@pkwteile.de>
2020-02-27 14:58:34 +0200

    qc_1091 fix

--- src/main/java/ATD/Austauschartikel_static_page_Logic.java
+++ src/main/java/ATD/Austauschartikel_static_page_Logic.java
@@ -78,6 +78,7 @@ public class Austauschartikel_static_page_Logic extends Austauschartikel_static_
         titleFromDepositRefundBlock().shouldBe(visible);
         informTextFromDepositRefundBlock().shouldBe(visible);
         depositRefundForm().shouldBe(visible);
+        plzTooltip().scrollTo();
         plzTooltip().click(10, 10);
         closePlzTooltip().click();
         numberTooltip().click();
