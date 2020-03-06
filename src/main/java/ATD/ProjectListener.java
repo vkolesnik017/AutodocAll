f4a7f04381a30241ee241251929425bb58ed64ca
Alexei <alexeym@pkwteile.de>
2020-03-06 13:32:45 +0200

    adding listeners to all xml

--- src/main/java/ATD/ProjectListener.java
+++ src/main/java/ATD/ProjectListener.java
@@ -17,7 +17,7 @@ import java.nio.file.Paths;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
-public class FailScreenshotListener implements ITestListener {
+public class ProjectListener implements ITestListener {
     @Override
     public void onTestStart(ITestResult iTestResult) {
 
