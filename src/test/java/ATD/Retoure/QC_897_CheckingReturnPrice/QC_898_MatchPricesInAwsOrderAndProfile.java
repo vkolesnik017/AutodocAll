6f98890d8a071e9e10b8f057c154cbf65e8377da
Alexei <alexeym@pkwteile.de>
2020-03-18 00:00:05 +0200

    retoure test adding close

--- src/test/java/ATD/Retoure/QC_897_CheckingReturnPrice/QC_898_MatchPricesInAwsOrderAndProfile.java
+++ src/test/java/ATD/Retoure/QC_897_CheckingReturnPrice/QC_898_MatchPricesInAwsOrderAndProfile.java
@@ -66,10 +66,12 @@ public class QC_898_MatchPricesInAwsOrderAndProfile {
         assertEquals(productPriceInAwsOrder, productPriceOnRetourenPage);
     }
 
-//    @AfterMethod
-//    public void setStatusTestToOrder() {
-//        new Order_aws(orderNumber).setStatusOrderToTestbestellungen();
-//        close();
-//    }
+
+@AfterMethod
+public void setStatusTestToOrder() {
+new Order_aws(orderNumber).setStatusOrderToTestbestellungen();
+close();
+}
+
 
 }
