a1002148b056b5137d4d16fd3065f62b46fa83da
Max <workhardem@gmail.com>
2020-03-18 13:28:03 +0200

    fix QC-735, QC-743

--- src/test/java/ATD/Selectors/QC_729_PopUpsOfSelectors/QC_735_PresenceOfElementsInCarSelectorPopup.java
+++ src/test/java/ATD/Selectors/QC_729_PopUpsOfSelectors/QC_735_PresenceOfElementsInCarSelectorPopup.java
@@ -1,5 +1,6 @@
 package ATD.Selectors.QC_729_PopUpsOfSelectors;
 
+import ATD.DataBase;
 import ATD.Main_page_Logic;
 import ATD.SetUp;
 import io.qameta.allure.Description;
@@ -12,6 +13,7 @@ import org.testng.annotations.Test;
 
 import java.sql.SQLException;
 
+import static ATD.CommonMethods.openPage;
 import static ATD.SetUp.setUpBrowser;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
@@ -21,6 +23,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class QC_735_PresenceOfElementsInCarSelectorPopup {
 
   private Main_page_Logic mainPageLogic = new Main_page_Logic();
+  private DataBase dataBase = new DataBase();
 
   @BeforeClass
   void setUp() {
@@ -29,9 +32,8 @@ public class QC_735_PresenceOfElementsInCarSelectorPopup {
 
   @DataProvider(name = "routes")
   Object[] dataProvider() throws SQLException {
-    return new SetUp().setUpShopWithSubroutes("prod", "DE", "main","main,category_car_list,product,listing_accessories");
+    return new SetUp().setUpShopWithSubroutes("prod", "DE", "main","main,category_car_list,product");
   }
-
 
   @Test(dataProvider = "routes")
   @Flaky
@@ -47,6 +49,23 @@ public class QC_735_PresenceOfElementsInCarSelectorPopup {
     mainPageLogic.linkForTooltipInCarSelectorPopup().hover();
     mainPageLogic.tooltipInCarSelectorPopup().shouldBe(visible);
   }
+
+  @Test
+  @Flaky
+  @Owner(value = "Evlentiev")
+  @Description(value = "Presence of elements in popup of selector on accessories route")
+  public void testPresenceOfElementsInPopupOfSelectorAccessories() throws SQLException {
+    openPage("https://autodoc.de/" + dataBase.getRouteByRouteName("DE", "category_car_list"));
+    open("https://autodoc.de/" + dataBase.getRouteByRouteName("DE", "listing_accessories3"));
+    mainPageLogic.fillNumberKba("0000", "000").clickKbaBtn();
+    mainPageLogic.headingInCarSelectorPopup().shouldHave(exactText("Ihre KBA Eingabe 0000.000 ergab keine Übereinstimmung mit unserer Datenbank"));
+    mainPageLogic.blockWithDropdownsOfChooseCarInCarSelectorPopup().shouldBe(visible);
+    mainPageLogic.headingCarInCarSelectorPopup().shouldHave(exactText("Oder stellen Sie sicher, dass Sie die KBA-Nummer korrekt eingeben"));
+    mainPageLogic.blockWithKbaFieldsInCarSelectorPopup().shouldBe(visible);
+    mainPageLogic.linkForTooltipInCarSelectorPopup().hover();
+    mainPageLogic.tooltipInCarSelectorPopup().shouldBe(visible);
+  }
+
   @AfterMethod
   private void tearDown() {
     close();
