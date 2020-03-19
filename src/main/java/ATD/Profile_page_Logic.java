3a3a9f00c0a37a341c47626d4609364c77e1d3fc
Roma <r.chelombitko@autodoc.eu>
2020-03-19 17:24:27 +0200

    Create test QC_1678_HeavyLoadsNegativeCasAllData and test QC_1679_HeavyLoadsFR_PositiveCase Refactoring QC_1676 and QC_1677

--- src/main/java/ATD/Profile_page_Logic.java
+++ src/main/java/ATD/Profile_page_Logic.java
@@ -135,4 +135,10 @@ public class Profile_page_Logic extends Profile_page {
         return this;
     }
 
+    @Step(":on Profile_page")
+    public Cart_page_Logic cartClick() {
+        new Main_page_Logic().cartClick();
+        return page(Cart_page_Logic.class);
+    }
+
 }
