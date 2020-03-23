c8e59c7ede6d6443d4e7466a5841ea52bf2d0633
Alexei-hub <00021@ua.fm>
2020-03-23 18:58:12 +0200

    test commit

--- src/test/java/ATD/ForTest.java
+++ src/test/java/ATD/ForTest.java
@@ -1,14 +1,17 @@
 package ATD;
 
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.sql.SQLException;
+
 import static ATD.SetUp.setUpBrowser;
-import static com.codeborne.selenide.Selenide.close;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+
 
 public class ForTest {
 
@@ -18,28 +21,25 @@ public class ForTest {
     }
 
     @DataProvider(name = "route")
-    Object[] dataProvider() {
-        return new SetUp().setUpShop("prod", "DE");
+    Object[] dataProvider() throws SQLException {
+        return new SetUp().setUpShopWithSubroutes("prod", "DE", "main", "product2");
     }
 
+    SelenideElement counterValue = $(By.xpath("//div[@class='count product-count']/input"));
+    SelenideElement counterPlus = $(By.xpath("//div[@class='count product-count']/a[2]"));
 
-    @Test()
-    public void testWithBug() {
-        open("https://github.com/cbeust/testng/issues/1241");
-        Assert.assertEquals(2, 3);
-        new CommonMethods().scrollToBlockOfTopProducts();
+    @Test(dataProvider = "route")
+    public void testRetryVisibility(String route) {
+        open(route);
+        new CommonMethods().checkingCounterIncrease(3, counterValue, counterPlus);
     }
 
-    @Test()
-    public void test() {
-        open("https://github.com/cbeust/testng/issues/1241");
-        Assert.assertEquals(2, 2);
-    }
+//    @AfterMethod
+//    private void teatDown() {
+//        close();
+//    }
 
-    @AfterMethod
-    private void teatDown() {
-        close();
-    }
+
 }
 
 
