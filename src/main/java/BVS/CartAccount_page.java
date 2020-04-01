1e399c588d567e947979d35dfc9103b06224e6ef
Roma <r.chelombitko@autodoc.eu>
2020-04-01 17:58:10 +0200

    Created Page Object for BVS skin for working with test set QC-1694 Added test QC_1696_CheckErrorTranslateOnAddressPage for BVS skin

---
+++ src/main/java/BVS/CartAccount_page.java
@@ -0,0 +1,22 @@
+package BVS;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byCssSelector;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+
+public class CartAccount_page {
+
+    SelenideElement emailFieldInLoginForm() {
+        return $(byXpath("//*[@class='loginForm']//*[@id='form_Email']"));
+    }
+
+    SelenideElement passwordFieldInLoginForm() {
+        return $(byXpath("//*[@class='loginForm']//*[@id='form_Password']"));
+    }
+
+    SelenideElement loginButton() {
+        return $(byCssSelector(".login"));
+    }
+}
