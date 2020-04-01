70a99b2a19184c24f3a61430be691bfbeff7e85c
Roma <r.chelombitko@autodoc.eu>
2020-04-01 15:02:43 +0200

    Created Page Object for Direkt skin for working with test set QC-1694 Added test QC_1696_CheckErrorTranslateOnAddressPage for Direkt skin

---
+++ src/main/java/Direkt/Cart_page.java
@@ -0,0 +1,11 @@
+package Direkt;
+
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class Cart_page {
+
+    SelenideElement nextButton() {
+        return $x("//a[@class='green ga-click']");
+    }
+}
