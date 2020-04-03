99b150a9174ae67915f04c3e06ed5096615da600
Roma <r.chelombitko@autodoc.eu>
2020-04-03 18:53:19 +0200

    Created Page Object for TFK skin for working with test set QC-1694 Added test QC_1696_CheckErrorTranslateOnAddressPage for TFK skin

---
+++ src/main/java/TKF/Cart_page.java
@@ -0,0 +1,12 @@
+package TKF;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class Cart_page {
+
+    SelenideElement nextButton() {
+        return $x("//a[@class='crumbs-button']");
+    }
+}
