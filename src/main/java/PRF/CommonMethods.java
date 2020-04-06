40e1cf52f5632d523907346082d5b97ceb4a0ffc
Alexei <alexeym@pkwteile.de>
2020-04-06 14:07:09 +0300

    prf change shop cf to fr

--- src/main/java/PRF/CommonMethods.java
+++ src/main/java/PRF/CommonMethods.java
@@ -13,7 +13,7 @@ public class CommonMethods {
 
     public static String getCurrentShopFromJSVarInHTML() {
         String currentShop = executeJavaScript("return $siteSettings.lang");
-        if(url().contains("piecesdetachees.ch")) currentShop = "ch";
+        if(url().contains("piecesdetachees.ch")) currentShop = "fr";
         return currentShop.toUpperCase();
     }
 
