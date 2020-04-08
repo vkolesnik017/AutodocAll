420fc1c3d816d2b691b28ed550dbbac5b96baab2
Roma <r.chelombitko@autodoc.eu>
2020-04-08 11:44:28 +0200

    Create test QC_1697_CheckOfNotBlockingOfIndexInTheBillingBlock for Ersatz skins

--- src/main/java/Ersatz/CartAddress_page.java
+++ src/main/java/Ersatz/CartAddress_page.java
@@ -11,12 +11,28 @@ import static com.codeborne.selenide.Selenide.$x;
 public class CartAddress_page {
 
 
-    SelenideElement countryInSelector(String country) {
+    SelenideElement countryInSelectorForShipping(String country) {
         return $(byXpath("//*[@name='lLand']//*[@data-code='" + country + "']"));
     }
 
-    SelenideElement postalCodeField() {
+    SelenideElement countryInSelectorForBilling(String country) {
+        return $(byXpath("//*[@name='rLand']//*[@data-code='" + country + "']"));
+    }
+
+    SelenideElement postalCodeFieldForShipping() {
         return $(By.id("form_lPlz"));
+    }
+
+    SelenideElement postalCodeFieldForBilling() {
+        return $(By.id("form_rPlz"));
+    }
+
+    SelenideElement billingCheckBox() {
+        return $x("//input[@id='showBilling']");
+    }
+
+    SelenideElement billingForm() {
+        return $(By.id("billing_form"));
     }
 
     SelenideElement nextButton() {
