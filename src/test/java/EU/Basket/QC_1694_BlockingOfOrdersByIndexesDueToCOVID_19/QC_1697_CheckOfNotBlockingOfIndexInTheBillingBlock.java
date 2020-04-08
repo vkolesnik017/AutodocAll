ed714dffca6b0f801f2b3b4ab6fa70cd7652d0a7
Roma <r.chelombitko@autodoc.eu>
2020-04-08 13:36:06 +0200

    Create test QC_1697_CheckOfNotBlockingOfIndexInTheBillingBlock for EU skins

---
+++ src/test/java/EU/Basket/QC_1694_BlockingOfOrdersByIndexesDueToCOVID_19/QC_1697_CheckOfNotBlockingOfIndexInTheBillingBlock.java
@@ -0,0 +1,57 @@
+package EU.Basket.QC_1694_BlockingOfOrdersByIndexesDueToCOVID_19;
+
+import EU.Product_page_Logic;
+import EU.SetUp;
+import io.qameta.allure.Description;
+import io.qameta.allure.Flaky;
+import io.qameta.allure.Owner;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.sql.SQLException;
+
+import static EU.SetUp.setUpBrowser;
+import static com.codeborne.selenide.Selenide.close;
+import static com.codeborne.selenide.Selenide.open;
+
+public class QC_1697_CheckOfNotBlockingOfIndexInTheBillingBlock {
+
+    private String email = "qc_1697_autotestCOVID19@mailinator.com";
+    private String password = "atdtest";
+
+    private String plzIT = "00017";
+    private String plzES = "10900";
+    private String plzPT = "3880-365";
+
+    @BeforeClass
+    void setUp() {
+        setUpBrowser(false, "chrome", "77.0");
+    }
+
+    @DataProvider(name = "route", parallel = false)
+    Object[] dataProviderProducts() throws SQLException {
+        return new SetUp().setUpShopsWithSubroute("prod", "DE", "main", "product");
+    }
+
+    @Test(dataProvider = "route")
+    @Flaky
+    @Owner(value = "Chelombitko")
+    @Description(value = "Test checks translation of error popup on address page")
+    public void testCheckErrorTranslateOnAddressPage(String route) {
+        open(route);
+        new Product_page_Logic().addProductToCart()
+                .cartClick()
+                .nextButtonClick()
+                .signIn(email, password)
+                .chooseDeliveryCountryAndFillingPostalCode("IT", plzIT, "IT", "12345")
+                .chooseDeliveryCountryAndFillingPostalCode("ES", plzES, "ES", "12345")
+                .chooseDeliveryCountryAndFillingPostalCode("PT", plzPT, "PT", "1234-567");
+    }
+
+    @AfterMethod
+    private void teatDown() {
+        close();
+    }
+}
