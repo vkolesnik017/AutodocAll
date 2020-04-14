2f957958c21fa8a1c081438eb0c20934580eaecf
Max <workhardem@gmail.com>
2020-04-14 20:20:11 +0300

    add QC-496

--- src/test/java/ATD/Characteristics/QC_486_DynamicCharacteristics/QC_496_DynamicCharacteristicNotDisplayedWithCarBrand.java
+++ src/test/java/ATD/Characteristics/QC_486_DynamicCharacteristics/QC_496_DynamicCharacteristicNotDisplayedWithCarBrand.java
@@ -6,6 +6,7 @@ import ATD.SetUp;
 import io.qameta.allure.Description;
 import io.qameta.allure.Flaky;
 import io.qameta.allure.Owner;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -14,6 +15,7 @@ import java.sql.SQLException;
 
 import static ATD.CommonMethods.openPage;
 import static ATD.SetUp.setUpBrowser;
+import static com.codeborne.selenide.Selenide.close;
 import static com.codeborne.selenide.Selenide.open;
 
 
@@ -37,5 +39,10 @@ public class QC_496_DynamicCharacteristicNotDisplayedWithCarBrand {
         openPage(route);
         open(new DataBase().getFullRouteByRouteAndSubroute("prod", "DE", "main", "product25"));
         new Product_page_Logic().checkDynamicCharacteristic();
+    }
+
+    @AfterMethod
+    private void tearDown() {
+        close();
     }
 }
