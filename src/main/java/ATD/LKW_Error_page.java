251e77f8c6e659a51793f0090e60ce92d45ad2f8
vkolesnik017 <vkolesnik017@gmail.com>
2020-05-13 19:47:57 +0300

    added QC_158, QC_209, QC_222

--- src/main/java/ATD/LKW_Error_page.java
+++ src/main/java/ATD/LKW_Error_page.java
@@ -7,4 +7,6 @@ import static com.codeborne.selenide.Selenide.$x;
 public class LKW_Error_page {
 
     SelenideElement headlineInHeader() {return $x("//div[@class='title_page']");}
+
+    SelenideElement mainErrorImage() {return $x("//div[@class='img']");}
 }
