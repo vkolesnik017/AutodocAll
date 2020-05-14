d3d56625a4a9c052cb78bc722a1805740cd57196
Alexei <alexeym@pkwteile.de>
2020-05-14 17:34:25 +0200

    Update Index_accessories_page_Logic.java

--- src/main/java/ATD/Index_accessories_page_Logic.java
+++ src/main/java/ATD/Index_accessories_page_Logic.java
@@ -14,7 +14,7 @@ public class Index_accessories_page_Logic extends Index_accessories_page {
     }
 
 
-    @Step("Checks the visibility of the delivery service block on the site page . Index_accessories_page")
+    @Step("Checks the visibility of the delivery service block. Index_accessories_page")
     public Index_accessories_page_Logic checkingVisibilityOfDeliveryBlock() {
         blockDelivery().shouldBe(visible);
         return this;
