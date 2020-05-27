f9d26f6d3985ef8d55a9ad73c86baea01a6f0af4
vkolesnik017 <52042636+vkolesnik017@users.noreply.github.com>
2020-05-27 18:55:22 +0300

    fix QC_306, update QC_103, finished QC_307

---
+++ src/main/java/ATD/Moto_Category_car_list_model_page.java
@@ -0,0 +1,22 @@
+package ATD;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class Moto_Category_car_list_model_page {
+
+    SelenideElement brandOfMotoField() {return $(byId("form_maker_id"));}
+
+    SelenideElement modelFiledInSelector() {
+        return $(byId("form_model_id"));
+    }
+
+    SelenideElement motorFiledInSelector() {
+        return $(byId("form_car_id"));
+    }
+
+    SelenideElement searchButton() {return $x("//a[contains(@class,'search_button')]");}
+}
