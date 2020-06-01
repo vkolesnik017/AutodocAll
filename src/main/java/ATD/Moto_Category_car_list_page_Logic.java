d18682e81127f7bdbed9e8fe89750cdaacc6ebf6
vkolesnik017 <52042636+vkolesnik017@users.noreply.github.com>
2020-06-01 18:19:47 +0300

    fix QC_99, QC_78; added QC_746, QC_748, QC_750, QC_751, QC_752, QC_384, QC_374; update QC_849

--- src/main/java/ATD/Moto_Category_car_list_page_Logic.java
+++ src/main/java/ATD/Moto_Category_car_list_page_Logic.java
@@ -1,10 +1,12 @@
 package ATD;
 
+import com.codeborne.selenide.CollectionCondition;
 import io.qameta.allure.Step;
 import org.testng.Assert;
 
 import java.sql.SQLException;
 
+import static com.codeborne.selenide.CollectionCondition.*;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.page;
 import static com.codeborne.selenide.WebDriverRunner.url;
@@ -40,14 +42,14 @@ public class Moto_Category_car_list_page_Logic extends Moto_Category_car_list_pa
     public Moto_Category_car_list_page_Logic checkCurrentUrl(String subRoute) throws SQLException {
         tecDocListingBlock().shouldBe(visible);
         DataBase db = new DataBase();
-        Assert.assertEquals(url(),db.getFullRouteByRouteAndSubroute("subprod", "DE", "moto_main", subRoute));
+        Assert.assertEquals(url(), db.getFullRouteByRouteAndSubroute("subprod", "DE", "moto_main", subRoute));
         return this;
     }
 
 
     @Step("get text from headline .Moto_Category_car_list_page")
     public String getTextFromHeadline() {
-             String textFromHeadline = headline().getText();
+        String textFromHeadline = headline().getText();
         return textFromHeadline;
     }
 
@@ -70,4 +72,47 @@ public class Moto_Category_car_list_page_Logic extends Moto_Category_car_list_pa
         return this;
     }
 
-  }
+
+    @Step("get amount count of products from TecDoc listing .Moto_Category_car_list_page")
+    public double getAmountCountOfProducts() {
+        double countOfProductsFromTecDoc = Double.parseDouble(countOfProduct().getText().replaceAll("[^0-9]", ""));
+        return countOfProductsFromTecDoc;
+    }
+
+
+    @Step("check amount count of pages at TecDoc listing .Moto_Category_car_list_page")
+    public Moto_Category_car_list_page_Logic checkCountOfPagesAtTecDocListing() {
+        String url = lastForwardOfPagination().getAttribute("href");
+        String urlPart = url.replace(url.substring(url.lastIndexOf("=")), "");
+        int lastPage = Integer.parseInt(url.replace(urlPart, "").replaceAll("[^0-9]", ""));
+        int countOfPagesFromAmountCountOfProducts = (int) Math.ceil(getAmountCountOfProducts() / 20);
+        Assert.assertEquals(countOfPagesFromAmountCountOfProducts, lastPage);
+        return this;
+    }
+
+
+    @Step(" check TecDoc listing on pages .Moto_Category_car_list_page")
+    public Moto_Category_car_list_page_Logic checkTecDocListingOnPages() {
+        presenceOfProductsAtTecDocListing();
+        while (lastForwardOfPagination().isDisplayed()) {
+            nextForwardOfPagination().click();
+            presenceOfProductsAtTecDocListing();
+        }
+        return this;
+    }
+
+    @Step(" presence of products at TecDoc listing.Moto_Category_car_list_page")
+    public Moto_Category_car_list_page_Logic presenceOfProductsAtTecDocListing() {
+        productsAtTecDocListing().shouldHave(sizeGreaterThan(1));
+        return this;
+    }
+
+
+    @Step(" presence of TecDoc listing block.Moto_Category_car_list_page")
+    public Moto_Category_car_list_page_Logic presenceOfTecDocListingBlock() {
+        tecDocListingBlock().shouldBe(visible);
+        productsAtTecDocListing().shouldHave(sizeGreaterThan(1));
+        return this;
+    }
+
+}
--- src/test/java/MOTO/QC_355_BenefitsBlock/QC_360_MainImageOfChildCategory.java
+++ src/test/java/MOTO/QC_355_BenefitsBlock/QC_360_MainImageOfChildCategory.java
@@ -5,6 +5,7 @@ import ATD.SetUp;
 import io.qameta.allure.Description;
 import io.qameta.allure.Flaky;
 import io.qameta.allure.Owner;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -13,6 +14,7 @@ import java.sql.SQLException;
 
 import static ATD.CommonMethods.openPage;
 import static ATD.SetUp.setUpBrowser;
+import static com.codeborne.selenide.Selenide.close;
 
 public class QC_360_MainImageOfChildCategory {
     @BeforeClass
@@ -34,5 +36,10 @@ public class QC_360_MainImageOfChildCategory {
 
         new Moto_Category_page_Logic()
                 .presenceOfMainImageAtChildCategory();
+    }
+
+    @AfterMethod
+    private void tearDown() {
+        close();
     }
 }
