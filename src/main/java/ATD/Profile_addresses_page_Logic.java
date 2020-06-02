72b6c684706702e8c8d29cb277c3113142b7f6df
Roma <r.chelombitko@autodoc.eu>
2020-06-02 15:19:30 +0200

    Finished test QC_928

--- src/main/java/ATD/Profile_addresses_page_Logic.java
+++ src/main/java/ATD/Profile_addresses_page_Logic.java
@@ -21,20 +21,20 @@ public class Profile_addresses_page_Logic extends Profile_addresses_page {
 
     @Step("Click shipping edit button. Profile_addresses_page")
     public Profile_addresses_page_Logic clickShippingEditButton() {
-        shippingEditButton().click();
+        deliveryEditButton().click();
         return this;
     }
 
     @Step("Checks presence shipping address form. Profile_addresses_page")
     public Profile_addresses_page_Logic checkPresenceShippingAddressForm() {
-        shippingAddressForm().shouldBe(visible);
+        deliveryAddressForm().shouldBe(visible);
         return this;
     }
 
     @Step("Checks absence shipping address form. Profile_addresses_page")
     public Profile_addresses_page_Logic checkAbsenceShippingAddressForm() {
         sleep(2000);
-        shippingAddressForm().shouldNotBe(visible);
+        deliveryAddressForm().shouldNotBe(visible);
         return this;
     }
 
@@ -42,6 +42,54 @@ public class Profile_addresses_page_Logic extends Profile_addresses_page {
     public Profile_addresses_page_Logic clickBackupCancelBtn() {
         sleep(2000);
         backupCancelBtn().click();
+        return this;
+    }
+
+    @Step("Checks for text {expectedText} in a block Top Title. Profile_addresses_page")
+    public Profile_addresses_page_Logic checkForTextInBlockTopTitle(String expectedText) {
+        topTitleBlock().shouldHave(text(expectedText));
+        return this;
+    }
+
+    @Step("Checks presence client ID. Profile_addresses_page")
+    public Profile_addresses_page_Logic checkPresenceClientID() {
+        clientID().shouldBe(visible);
+        return this;
+    }
+
+    @Step("Checks presence heder private room block and the elements inside. Profile_addresses_page")
+    public Profile_addresses_page_Logic checkPresenceHeaderPrivateRoomBlockAndElementInside() {
+        headerPrivateRoomBlock().shouldBe(visible);
+        nameOfClient().shouldBe(visible);
+        depositResultLabel().shouldBe(visible);
+        depositAmount().shouldBe(visible);
+        return this;
+    }
+
+    @Step("Checks name page {expectedName} and presence icon. Profile_addresses_page")
+    public Profile_addresses_page_Logic checkNamePageAndPresenceIcon(String expectedName) {
+        namePage().shouldBe(visible);
+        namePage().shouldHave(text(expectedName));
+        return this;
+    }
+
+    @Step("Checks billing address block and its elements. Profile_addresses_page")
+    public Profile_addresses_page_Logic checkBillingAddressBlockAndItsElement() {
+        billingBlockName().shouldBe(visible);
+        billingAddressBlock().shouldBe(visible);
+        billingEditButton().shouldBe(visible);
+        deleteBillingAddressBtn().shouldBe(visible);
+        addBillingAddressBtn().shouldBe(visible);
+        return this;
+    }
+
+    @Step("Checks delivery address block and its elements. Profile_addresses_page")
+    public Profile_addresses_page_Logic checkDeliveryAddressBlockAndItsElement() {
+        deliveryBlockName().shouldBe(visible);
+        deliveryAddressBlock().shouldBe(visible);
+        deliveryEditButton().shouldBe(visible);
+        deleteDeliveryAddressBtn().shouldBe(visible);
+        addDeliveryAddressBtn().shouldBe(visible);
         return this;
     }
 }
