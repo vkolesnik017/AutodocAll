561e358fcf6e602476a42e8d73f6491b8d7a224a
vkolesnik017 <52042636+vkolesnik017@users.noreply.github.com>
2020-06-23 14:06:18 +0300

    update QC_628, QC_629

--- src/main/java/ATD/Moto_Catalog_page_Logic.java
+++ src/main/java/ATD/Moto_Catalog_page_Logic.java
@@ -273,7 +273,8 @@ public class Moto_Catalog_page_Logic extends Moto_Catalog_page {
     public Moto_Category_car_list_page_Logic clickOnChildCategory() {
         parentsCategoriesOfTecDocCatalog().get(0).click();
         childCategoriesFirstLevelBlock().get(0).shouldBe(visible);
-        childCategoriesFirstLevel(1).get(0).shouldBe(visible).click();
+      //  childCategoriesFirstLevel(1).get(0).shouldBe(visible).click();
+        chCategory().get(0).shouldBe(visible).click();
         return page(Moto_Category_car_list_page_Logic.class);
     }
 
