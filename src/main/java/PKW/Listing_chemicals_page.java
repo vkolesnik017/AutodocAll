7fedebf524f6552cbca4040696e77e9f1da1be63
wlanger54 <wlanger54@gmail.com>
2020-06-29 17:35:31 +0300

    Create tests QC-1746, QC-1795, QC-1761

--- src/main/java/PKW/Listing_chemicals_page.java
+++ src/main/java/PKW/Listing_chemicals_page.java
@@ -10,4 +10,16 @@ public class Listing_chemicals_page {
     SelenideElement titleNameListingPage() {
         return $x("//h1/span");
     }
+
+    SelenideElement firstBreadCrumb() {
+        return $x("//div[@class='crabs']//div[@style='float:left;'][1]");
+    }
+
+    SelenideElement secondBreadCrumb() {
+        return $x("//div[@class='crabs']//div[@style='float:left;'][2]");
+    }
+
+    SelenideElement thirdBreadCrumb() {
+        return $x("//div[@class='crabs']//a[@class='defcur']");
+    }
 }
--- src/main/java/PKW/Listing_chemicals_page_Logic.java
+++ src/main/java/PKW/Listing_chemicals_page_Logic.java
@@ -1,12 +1,33 @@
 package PKW;
 
+
 import io.qameta.allure.Step;
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.page;
 
 public class Listing_chemicals_page_Logic extends Listing_chemicals_page {
 
     @Step("Get title name listing page. Listing_chemicals_page")
     public String getTitleNameListingPage() {
        return titleNameListingPage().getText();
+    }
+
+    @Step("Click first bread crumb. Listing_chemicals_page")
+    public Parts_page_Logic clickFirstBreadCrumb() {
+        firstBreadCrumb().click();
+        return page(Parts_page_Logic.class);
+    }
+    @Step("Click second bread crumb. Listing_chemicals_page")
+    public Index_chemicals_page_Logic clickSecondBreadCrumb() {
+        secondBreadCrumb().click();
+        return page(Index_chemicals_page_Logic.class);
+    }
+
+    @Step("Checking presence and not clickable third bread crumb. Listing_chemicals_page")
+    public Listing_chemicals_page_Logic checkingPresenceAndNotClickableThirdBreadCrumb() {
+        thirdBreadCrumb().shouldBe(visible).shouldNotBe(attribute("href"));
+        return this;
     }
 
 }
