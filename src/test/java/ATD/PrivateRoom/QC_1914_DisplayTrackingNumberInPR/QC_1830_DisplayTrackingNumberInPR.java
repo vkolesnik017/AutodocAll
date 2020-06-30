afd0392695358a6af9694b4a5baf22915b8c3da8
Roma <r.chelombitko@autodoc.eu>
2020-06-30 17:07:22 +0200

    Added test QC_1830_DisplayTrackingNumberInPR Updated version of selenide from 5.3.1 to 5.5.1

---
+++ src/test/java/ATD/PrivateRoom/QC_1914_DisplayTrackingNumberInPR/QC_1830_DisplayTrackingNumberInPR.java
@@ -0,0 +1,93 @@
+package ATD.PrivateRoom.QC_1914_DisplayTrackingNumberInPR;
+
+import ATD.*;
+import AWS.Order_aws;
+import io.qameta.allure.Description;
+import io.qameta.allure.Flaky;
+import io.qameta.allure.Owner;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.sql.SQLException;
+
+import static ATD.CommonMethods.openPage;
+import static ATD.SetUp.setUpBrowser;
+import static com.codeborne.selenide.Selenide.close;
+
+public class QC_1830_DisplayTrackingNumberInPR {
+
+
+    private String mail = "QC_1830_autotest@mailinator.com", orderNumber;
+    private Main_page_Logic main_page_logic = new Main_page_Logic();
+
+    @BeforeClass
+    void setUp() {
+        setUpBrowser(false, "chrome", "77.0");
+    }
+
+    @DataProvider(name = "route", parallel = true)
+    Object[] dataProviderProducts() throws SQLException {
+        return new SetUp().setUpShop("prod", "DE");
+    }
+
+    @Test(dataProvider = "route")
+    @Flaky
+    @Owner(value = "Chelombitko")
+    @Description(value = "Test checks  the display of the tracking number in PR")
+    public void testDisplayTrackingNumberInPR(String route) throws SQLException {
+        openPage(route);
+        main_page_logic.loginAndTransitionToProfilePlusPage(mail);
+        openPage(new DataBase().getFullRouteByRouteAndSubroute("prod", "DE", "main", "product32"));
+        new Product_page_Logic().addProductToCart()
+                .closePopupOtherCategoryIfYes()
+                .cartClick()
+                .clickBtnNextAndTransitionOnAddressPage()
+                .fillFieldTelNumForShipping("100+001")
+                .nextBtnClick()
+                .chooseVorkasse()
+                .nextBtnClick()
+                .nextBtnClick();
+        orderNumber = new Payment_handler_page_Logic().getOrderNumber();
+        Order_aws order_aws = new Order_aws(orderNumber);
+        order_aws.openOrderInAwsWithLogin()
+                .checkCurrentStatusInOrder("Neue Bestellung")
+                .selectDeliveryAndEnterTrackingNum("DHL", "0", "0", "1111111111")
+                .selectDeliveryAndEnterTrackingNum("DHL", "1", "1", "2222222222")
+                .selectDeliveryAndEnterTrackingNum("DHL", "2", "2", "3333333333")
+                .selectDeliveryAndEnterTrackingNum("DHL", "3", "3", "4444444444")
+                .selectDeliveryAndEnterTrackingNum("DHL", "4", "4", "5555555555")
+                .selectStatusOrder("Versendet")
+                .saveOrder()
+                .checkCurrentStatusInOrder("Versendet");
+        openPage(route);
+        main_page_logic.profilePlusBtnClickInHeader()
+                .goToMyOrdersPage()
+                .checkPresenceDeliveryStatusBlock()
+                .checkNumberDeliveryServiceAdded(5)
+                .checkTransitionToDeliveryPage();
+        order_aws.openOrderInAwsWithoutLogin()
+                .checkCurrentStatusInOrder("Versendet")
+                .clickBtnAddedDeliveryInOrderBtn()
+                .selectDeliveryAndEnterTrackingNum("DHL", "6", "6", "6666666666")
+                .selectStatusOrder("Versendet")
+                .saveOrder()
+                .checkCurrentStatusInOrder("Versendet");
+        openPage(route);
+        main_page_logic.profilePlusBtnClickInHeader()
+                .goToMyOrdersPage()
+                .checkPresenceDeliveryStatusBlock()
+                .checkNumberDeliveryServiceAddedFromTooltip(5)
+                .trackingNumInDeliveryPageFromTooltip();
+        order_aws.openOrderInAwsWithoutLogin()
+                .checkCurrentStatusInOrder("Versendet")
+                .reSaveOrder()
+                .checkCurrentStatusInOrder("Testbestellungen");
+    }
+
+    @AfterMethod
+    private void tearDown() {
+        close();
+    }
+}
