964cb4cb3d7f4c022e22cdc5395d16708819aa01
wlanger54 <wlanger54@gmail.com>
2020-07-07 10:05:32 +0300

    Create tests QC-1794, QC-1816

--- src/main/java/PKW/Listing_chemicals_page.java
+++ src/main/java/PKW/Listing_chemicals_page.java
@@ -11,6 +11,8 @@ public class Listing_chemicals_page {
         return $x("//h1/span");
     }
 
+    SelenideElement blockBreadCrumbs() {return $x("//div[@class='crabs']"); }
+
     SelenideElement firstBreadCrumb() {
         return $x("//div[@class='crabs']//div[@style='float:left;'][1]");
     }
@@ -22,4 +24,29 @@ public class Listing_chemicals_page {
     SelenideElement thirdBreadCrumb() {
         return $x("//div[@class='crabs']//a[@class='defcur']");
     }
+
+    SelenideElement blockPagination() {
+        return $x("//div[@class='pagination_wrap']");
+    }
+
+    SelenideElement btnForReturnOnFirstPageInPagination() {
+        return $x("//div[@class='pagination']/span[@class='first']");
+    }
+
+    SelenideElement btnPreviousInPagination() {
+        return $x("//div[@class='pagination']/span[@class='previous']");
+    }
+
+    SelenideElement btnNextInPagination() {
+        return $x("//div[@class='pagination']/span[@class='next'][1]");
+    }
+
+    SelenideElement btnLastInPagination() {
+        return $x("//div[@class='pagination']/span[@class='next'][2]");
+    }
+
+    SelenideElement btnSecondPageInPagination() {
+    return $x("//div[@class='pagination']/span[3]");
+    }
+
 }
--- src/main/java/PKW/Listing_chemicals_page_Logic.java
+++ src/main/java/PKW/Listing_chemicals_page_Logic.java
@@ -2,6 +2,7 @@ package PKW;
 
 
 import io.qameta.allure.Step;
+import static PKW.CommonMethods.checkingContainsUrl;
 import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.page;
@@ -29,5 +30,33 @@ public class Listing_chemicals_page_Logic extends Listing_chemicals_page {
         thirdBreadCrumb().shouldBe(visible).shouldNotBe(attribute("href"));
         return this;
     }
+
+    @Step("Checking presence bread crumbs block. Listing_chemicals_page")
+    public Listing_chemicals_page_Logic checkingPresenceBreadCrumbsBlock() {
+        blockBreadCrumbs().shouldBe(visible);
+        return this;
+    }
+
+    @Step("Checking work buttons in pagination. Listing_chemicals_page")
+    public  Listing_chemicals_page_Logic checkingWorkBtnInPagination () {
+        btnSecondPageInPagination().scrollIntoView(false).click();
+        btnForReturnOnFirstPageInPagination().scrollIntoView(false).shouldBe(visible);
+        btnPreviousInPagination().shouldBe(visible);
+        btnNextInPagination().shouldBe(visible);
+        btnLastInPagination().shouldBe(visible);
+        btnPreviousInPagination().click();
+        blockPagination().scrollIntoView(false);
+        btnForReturnOnFirstPageInPagination().shouldNotBe(visible);
+        btnNextInPagination().click();
+        checkingContainsUrl("page=2");
+        btnLastInPagination().scrollIntoView(false).click();
+        blockPagination().scrollIntoView(false);
+        btnLastInPagination().shouldNotBe(visible);
+        btnForReturnOnFirstPageInPagination().click();
+        blockPagination().scrollIntoView(false);
+        btnForReturnOnFirstPageInPagination().shouldNotBe(visible);
+        return this;
+    }
+
 
 }
