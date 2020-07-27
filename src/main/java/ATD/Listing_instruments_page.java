3138aeaac8a6c813de8e31f86dbb9d69624419c1
wlanger54 <wlanger54@gmail.com>
2020-07-27 17:58:02 +0300

    Create tests QC_2117, QC_2151, QC_2150

--- src/main/java/ATD/Listing_instruments_page.java
+++ src/main/java/ATD/Listing_instruments_page.java
@@ -9,4 +9,28 @@ public class Listing_instruments_page {
         return $x("//h2[@class='title_count_search']");
     }
 
+    SelenideElement seoTextBlock() {
+        return $x("//div[@class='block_youtube_video']");
+    }
+
+    SelenideElement btnReturnOnFirstPageInPagination() {
+        return $x("//div[@class='page_list'][1]//span[@class='first']");
+    }
+
+    SelenideElement btnPreviousPageInPagination() {
+        return $x("//div[@class='page_list'][1]//span[contains(@class,'previous')]");
+    }
+
+    SelenideElement btnNextPageInPagination() {
+        return $x("//div[@class='page_list'][1]//span[@class='next'][1]");
+    }
+
+    SelenideElement btnLastPageInPagination() {
+        return $x("//div[@class='page_list'][1]//span[@class='next'][2]");
+    }
+
+    SelenideElement btnSecondPageInPagination() {
+        return $x("//div[@class='page_list'][1]//span[3]");
+    }
+
 }
