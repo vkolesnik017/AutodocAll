24ea231734c41317cf6db1be24784d1711e99e6c
vkolesnik017 <52042636+vkolesnik017@users.noreply.github.com>
2020-08-12 17:21:47 +0300

    added  QC-941, QC-954, QC-955, QC-945, QC-946, QC-947, QC-948

--- src/main/java/PKW/Motoroil_page.java
+++ src/main/java/PKW/Motoroil_page.java
@@ -1,10 +1,37 @@
 package PKW;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class Motoroil_page {
 
    SelenideElement brandsBlock() {return $x("//div[@class='featured_manufacturers featured_manufacturers--oil']");}
+
+   ElementsCollection brandLinks() {return $$x("//ul[@class='featured_manufacturers_image']/li/a");}
+
+   SelenideElement toleranceBlock() {return $x("//h4[contains(text(),'Herstellerfreigabe')]/..");}
+
+   ElementsCollection toleranceLinks() {return $$x("//h4[contains(text(),'Herstellerfreigabe')]/..//li/a");}
+
+   SelenideElement viscosityBlock() {return $x("//div[@class='select_oil_block--oil']");}
+
+   ElementsCollection linksOfViscosityBlock() {return $$x("//div[@class='select_oil_block--oil']//a");}
+
+   ElementsCollection iconOfLinksInViscosityBlock() {return $$x("//div[@class='select_oil_block--oil']//a/div[1]");}
+
+   SelenideElement topProductsBlock() {return $x("//div[@class='car_tires_toppop']");}
+
+   ElementsCollection linksOfTopProducts() {return $$x("//div[@class='car_tires_toppop']//ul/li");}
+
+   ElementsCollection btnDetailsOfTopProducts() {return $$x("//div[@class='pkw-related__additional-header']/span");}
+
+   ElementsCollection arrowsDownOfTopProduct() {return $$x("//div[@class='pkw-related__item-header']/button");}
+
+   ElementsCollection titleOfTopProducts() {return $$x("//span[@class='pkw-related__title']");}
+
+   ElementsCollection imageOfTopProducts() {return $$x("//span[@class='pkw-related__img-container']/img");}
+
 }
