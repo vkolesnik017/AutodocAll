a7110fc6bea19fefbcd1ee33c51f1d0f1a7fa5c5
@o.lavrinenko <o.lavrinenko@autodoc.eu>
2020-08-18 16:48:13 +0300

    finished test QC-1920, fix tests QC-1917, QC-1922

---
+++ src/test/java/PKW/General_Common/QC_1920_Header/QC_1920_PresenceOfTheBlocksInPromotionHeader.java
@@ -0,0 +1,38 @@
+package PKW.General_Common.QC_1920_Header;
+
+import PKW.Main_page_Logic;
+import PKW.SetUp;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import static PKW.CommonMethods.openPage;
+import static PKW.SetUp.setUpBrowser;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class QC_1920_PresenceOfTheBlocksInPromotionHeader {
+    @BeforeClass
+    void setUp() {
+        setUpBrowser(false, "chrome", "77.0");
+    }
+
+    @DataProvider(name = "route", parallel = true)
+    Object[] dataProvider() {
+        return new SetUp().setUpShop("prod", "DE");
+    }
+
+    @Test(dataProvider = "route")
+    @Owner(value = "LavrynenkoOlha")
+    @Description(value = "Test checks the elements of the promotion block in the header")
+    public void testElementsInPromotionBlock(String route) {
+        openPage(route);
+        new Main_page_Logic().checkHeaderPromotionElements();
+    }
+
+    @AfterMethod
+    private void close() {
+        closeWebDriver();
+    }
+}
