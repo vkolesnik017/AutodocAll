720a5b0236245f5f6e9320b46f0f0f9dcf51fe96
Roma <r.chelombitko@autodoc.eu>
2020-08-21 15:09:28 +0300

    Finished test QC-2165

---
+++ src/test/java/ATD/Payments/QC_2165_Bancontact_Mister_Cash.java
@@ -0,0 +1,83 @@
+package ATD.Payments;
+
+import ATD.*;
+import AWS.Customer_view_aws;
+import AWS.Order_aws;
+import io.qameta.allure.Description;
+import io.qameta.allure.Flaky;
+import io.qameta.allure.Owner;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.sql.SQLException;
+
+import static ATD.CommonMethods.*;
+import static ATD.CommonMethods.checkingContainsUrl;
+import static ATD.DataBase.parseUserIdFromBD;
+import static ATD.DataBase.parseUserMailFromBD;
+import static ATD.SetUp.setUpBrowser;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class QC_2165_Bancontact_Mister_Cash {
+
+    private CartAllData_page_Logic cartAllData_page_logic = new CartAllData_page_Logic();
+
+    @BeforeClass
+    void setUp() {
+        setUpBrowser(false, "chrome", "77.0");
+    }
+
+    @DataProvider(name = "route", parallel = true)
+    Object[] dataProviderProducts() throws SQLException {
+        return new SetUp().setUpShopWithSubroutes("prod", "BE", "main", "product32");
+    }
+
+    @Test(dataProvider = "route")
+    @Flaky
+    @Owner(value = "Chelombitko")
+    @Description("Test checks method of payment by Bancontact Mister Cash")
+    public void testBancontactMisterCash(String route) throws Exception {
+        openPage(route);
+        String shop = getCurrentShopFromJSVarInHTML();
+        String userData = new DataBase().getUserIdForPaymentsMethod("payments_userid_atd", shop, "Bancontact/Mister Cash");
+        String userID = parseUserIdFromBD(userData);
+        String mail = parseUserMailFromBD(userData);
+        float totalPriceAllData = new Product_page_Logic().addProductToCart()
+                .closePopupOtherCategoryIfYes()
+                .cartClick()
+                .checkPresencePaymentsMethodLabel(new Cart_page_Logic().masterCashLabel())
+                .nextButtonClick()
+                .signIn(mail, passwordForPayments)
+                .chooseDeliveryCountryForShipping(shop)
+                .fillFieldTelNumForShipping("100+001")
+                .nextBtnClick()
+                .clickOnTheDesiredPaymentMethod(shop, "Bancontact/Mister Cash")
+                .nextBtnClick()
+                .checkPresencePaymentsMethodLabel(new CartAllData_page().masterCashLabel())
+                .getTotalPriceAllDataPage(shop);
+        cartAllData_page_logic.nextBtnClick();
+        checkingContainsUrl("/be2bill");
+        cartAllData_page_logic.fillsInFieldsForEnteringDataAndCancelsPayment("11111111", "11", "11");
+        checkingContainsUrl("/basket/payments.html");
+        new CartPayments_page_Logic().checkActivePaymentMethod("be2bill_mistercash");
+        float totalPriceOrderAws = new Customer_view_aws().openCustomerPersonalArea(userID)
+                .checkPresenceOrderHistoryBlock()
+                .checkAndOpenOrderWithExpectedData()
+                .checkPaymentMethodInOrder("Mister Cash - Be2bill")
+                .checkCurrentStatusInOrder("abgebrochene Be2bill")
+                .getTotalPriceOrderAWS();
+        Assert.assertEquals(totalPriceAllData, totalPriceOrderAws);
+        float totalPriceOrderAwsAfterReSave = new Order_aws().reSaveOrder()
+                .checkCurrentStatusInOrder("Testbestellungen")
+                .getTotalPriceOrderAWS();
+        Assert.assertEquals(totalPriceAllData, totalPriceOrderAwsAfterReSave);
+    }
+
+    @AfterMethod
+    private void close() {
+        closeWebDriver();
+    }
+}
