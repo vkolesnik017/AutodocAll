5a1284df70e7e7a49401fd8c1cec8037903d281a
wlanger54 <wlanger54@gmail.com>
2020-08-26 17:33:44 +0300

    Create tests qc-2296, qc-2295, qc-2298, qc-2232, qc-2234

--- src/main/java/PKW/Listing_instruments_Page.java
+++ src/main/java/PKW/Listing_instruments_Page.java
@@ -1,6 +1,9 @@
 package PKW;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
 
@@ -9,4 +12,37 @@ public class Listing_instruments_Page {
     SelenideElement titleCategory() {
         return $x("//h1/span");
     }
+
+    SelenideElement firstBrandInBrandsBlock() {
+        return $x("//div[contains(@class,'listing-brand-filters__brand')][1]");
+    }
+
+    SelenideElement thirdBrandInBrandsBlock() {
+        return $x("//div[contains(@class,'listing-brand-filters__brand')][3]");
+    }
+
+    SelenideElement firstBrandInBrandsBlockImg() {
+        return $x("//div[@class='listing-brand-filters__list-brands delay-overflow']//div[1]//img");
+    }
+
+    SelenideElement thirdBrandInBrandsBlockImg() {
+        return $x("//div[@class='listing-brand-filters__list-brands delay-overflow']//div[3]//img");
+    }
+
+    SelenideElement brandsBlock() {
+        return $x("//div[@class='listing-brand-filters__list-brands delay-overflow']");
+    }
+
+    ElementsCollection titleNameProductsFromListing() {
+        return $$x("//div[@class='brand-products']//*[contains(@class,'ga-click prod_link')]");
+    }
+
+    SelenideElement btnMoreInBrandsBlock() {
+        return $x("//div[contains(@class,'listing-brand-filters_more-link')]/a[@class='more_link js-listing-brand-filters']");
+    }
+
+    SelenideElement btnLessInBrandsBlock() {
+        return $x("//div[contains(@class,'listing-brand-filters_more-link')]/a[@class='more_link js-listing-brand-filters less_link']");
+    }
+
 }
