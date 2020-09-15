095c5f1ae418b9875c777e1dc33d1869c3e422dd
Roma <r.chelombitko@autodoc.eu>
2020-09-15 14:35:52 +0300

    fix QC-1015, QC-1025

--- src/main/java/AWS/WishlistReminderAvailability_aws.java
+++ src/main/java/AWS/WishlistReminderAvailability_aws.java
@@ -1,5 +1,6 @@
 package AWS;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
@@ -13,90 +14,84 @@ import static org.testng.Assert.assertEquals;
 
 public class WishlistReminderAvailability_aws {
 
-  private String currentDate = new SimpleDateFormat("yyyy/MM/dd").format(new Date()).replace("/", "-");
+    private String currentDate = new SimpleDateFormat("yyyy/MM/dd").format(new Date()).replace("/", "-");
 
 
-  public String urlWithCurrentDate = "https://aws.autodoc.de/wishlist/reminder/availability?Filter%5Bproject%5D%5B0%5D=_" + "DE"
-          + "&Filter%5Bdate%5D%5BdateFrom%5D=" + currentDate + "&Filter%5Bdate%5D%5BdateTo%5D=" + currentDate
-          + "&submit=%D0%9F%D0%BE%D0%B8%D1%81%D0%BA"; // TODO будем ли выносить в базу ?
+    public String urlWithCurrentDate = "https://aws.autodoc.de/wishlist/reminder/availability?Filter%5Bproject%5D%5B0%5D=_" + "DE"
+            + "&Filter%5Bdate%5D%5BdateFrom%5D=" + currentDate + "&Filter%5Bdate%5D%5BdateTo%5D=" + currentDate
+            + "&submit=%D0%9F%D0%BE%D0%B8%D1%81%D0%BA"; // TODO будем ли выносить в базу ?
 
 
-  public SelenideElement firstRowInTable() {
-    return $x(".//*[@id='availability_reminder_table']//tbody/tr[1]");
-  }
-
-  public SelenideElement articleOfFirstProduct() {
-    return $(byXpath(".//*[@id='availability_reminder_table']//tr[1]/td[3]"));
-  }
-
-  public SelenideElement articleOfSecondProduct() {
-    return $(byXpath(".//*[@id='availability_reminder_table']//tr[2]/td[3]"));
-  }
-
-  public SelenideElement nameCategoryInTableInFirstRow(String nameFirstCategories) {
-    return $x(".//*[@id='availability_reminder_table']//tbody/tr[1]/td[2]/a[text()='" + nameFirstCategories + "']");
-  }
-
-  public SelenideElement numberOfRequestsInFirstProduct() {
-    return $(byXpath(".//*[@id='availability_reminder_table']//tr[1]/td[6]"));
-  }
-
-  public SelenideElement numberOfRequestsInSecondProduct() {
-    return $(byXpath(".//*[@id='availability_reminder_table']//tr[2]/td[6]"));
-  }
-
-  public SelenideElement numberOfRequestsInProductByHisId(String idProduct) {
-    return $(byXpath("//td[contains(text(),'" + idProduct + "')]/../td[6]"));
-  }
-
-  public SelenideElement idOfFirstProduct() {
-    return $(byXpath(".//*[@id='availability_reminder_table']//tr[1]/td[4]"));
-  }
-
-  public SelenideElement idOfSecondProduct() {
-    return $(byXpath(".//*[@id='availability_reminder_table']//tr[2]/td[4]"));
-  }
-
-  @Step("Check After Count Request. WishlistReminderAvailability_aws")
-  public WishlistReminderAvailability_aws checkAfterCountRequest(int beforeCountRequests, String idProduct) {
-    int afterCountRequests = Integer.parseInt(numberOfRequestsInProductByHisId(idProduct).text());
-    assertEquals(afterCountRequests, beforeCountRequests + 1);
-    return this;
-  }
-
-  @Step("Get text from article. WishlistReminderAvailability_aws")
-  public String getTextFromArticle(String exclusionCategoryName) {
-    String textFromElement = null;
-    firstRowInTable().waitUntil(visible, 5000);
-    if (nameCategoryInTableInFirstRow(exclusionCategoryName).isDisplayed()) {
-      textFromElement = articleOfSecondProduct().text();
-    } else {
-      textFromElement = articleOfFirstProduct().text();
+    public SelenideElement firstRowInTable() {
+        return $x(".//*[@id='availability_reminder_table']//tbody/tr[1]");
     }
-    return textFromElement;
-  }
 
-  @Step("Get text from id. WishlistReminderAvailability_aws")
-  public String getTextFromId(String exclusionCategoryName) {
-    String textFromElement = null;
-    firstRowInTable().waitUntil(visible, 5000);;
-    if (nameCategoryInTableInFirstRow(exclusionCategoryName).isDisplayed()) {
-      textFromElement = idOfSecondProduct().text();
-    } else {
-      textFromElement = idOfFirstProduct().text();
+    public SelenideElement articleCategoryInTable(String nameCategories) {
+        return $x("//*[@id='availability_reminder_table']//tbody//td[2]//a[text()='" + nameCategories + "']/../..//td[3]");
     }
-    return textFromElement;
-  }
 
-  @Step("Get Before Count Requests. WishlistReminderAvailability_aws")
-  public int getBeforeCountRequests(String exclusionCategoryName) {
-    String numberOfRequests = null;
-    firstRowInTable().waitUntil(visible, 5000);;
-    if (nameCategoryInTableInFirstRow(exclusionCategoryName).isDisplayed()) {
-      numberOfRequests = numberOfRequestsInSecondProduct().text();
-    } else {
-      numberOfRequests = numberOfRequestsInFirstProduct().text();
+    public SelenideElement idCategoryInTable(String nameCategories) {
+        return $x("//*[@id='availability_reminder_table']//tbody//td[2]//a[text()='" + nameCategories + "']/../..//td[4]");
     }
-    return Integer.parseInt(numberOfRequests);
-  }
+
+    public ElementsCollection columnNameCategory() {
+        return $$x("//*[@id='availability_reminder_table']//tbody/tr/td[2]");
+    }
+
+    public SelenideElement numberOfRequests(String numberOfRequest) {
+        return $(byXpath("//*[@id='availability_reminder_table']//tbody//td[2]//a[text()='" + numberOfRequest + "']/../..//td[6]"));
+    }
+
+    public SelenideElement numberOfRequestsInProductByHisId(String idProduct) {
+        return $(byXpath("//td[contains(text(),'" + idProduct + "')]/../td[6]"));
+    }
+
+    @Step("Check After Count Request. WishlistReminderAvailability_aws")
+    public WishlistReminderAvailability_aws checkAfterCountRequest(int beforeCountRequests, String idProduct) {
+        int afterCountRequests = Integer.parseInt(numberOfRequestsInProductByHisId(idProduct).text());
+        assertEquals(afterCountRequests, beforeCountRequests + 1);
+        return this;
+    }
+
+    @Step("Get text from article. WishlistReminderAvailability_aws")
+    public String getTextFromArticle() {
+        String textFromElement = null;
+        firstRowInTable().waitUntil(visible, 5000);
+        for (int i = 0; i < columnNameCategory().size(); i++) {
+            String nameCategory = columnNameCategory().get(i).getText();
+            if (!nameCategory.contains("Tyres")) {
+                textFromElement = articleCategoryInTable(nameCategory).getText();
+                break;
+            }
+        }
+        return textFromElement;
+    }
+
+    @Step("Get text from id. WishlistReminderAvailability_aws")
+    public String getTextFromId() {
+        String textFromElement = null;
+        firstRowInTable().waitUntil(visible, 5000);
+        for (int i = 0; i < columnNameCategory().size(); i++) {
+            String nameCategory = columnNameCategory().get(i).getText();
+            if (!nameCategory.contains("Tyres")) {
+                textFromElement = idCategoryInTable(nameCategory).getText();
+                break;
+            }
+        }
+        return textFromElement;
+    }
+
+    @Step("Get Before Count Requests. WishlistReminderAvailability_aws")
+    public int getBeforeCountRequests() {
+        String numberOfRequests = null;
+        firstRowInTable().waitUntil(visible, 5000);
+        for (int i = 0; i < columnNameCategory().size(); i++) {
+            String nameCategory = columnNameCategory().get(i).getText();
+            if (!nameCategory.contains("Tyres")) {
+                numberOfRequests = numberOfRequests(nameCategory).getText();
+                break;
+            }
+        }
+        return Integer.parseInt(numberOfRequests);
+    }
 }
