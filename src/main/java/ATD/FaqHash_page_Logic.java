178c8755296b7c41a772ec65429a0236f4afd28d
vkolesnik017 <52042636+vkolesnik017@users.noreply.github.com>
2020-09-15 12:34:06 +0300

    update QC_1305. added QC_615 (disabled)

--- src/main/java/ATD/FaqHash_page_Logic.java
+++ src/main/java/ATD/FaqHash_page_Logic.java
@@ -28,13 +28,14 @@ public class FaqHash_page_Logic extends FaqHash_page {
     @Step("get exaсt SubCategories. FaqHash_page")
     public List<String> getExaсtSubCategories(List<String> list) {
         List<String> subCategories = new ArrayList<>();
+
         for (int i = 0; i < titleOfParentCategories().size(); i++) {
             if (list.contains(titleOfParentCategories().get(i).getText())) {
                 continue;
             } else {
                 titleOfParentCategories().get(i).click();
                 for (int j = 0; j < activeSubCategories().size(); j++) {
-                    subCategories.add(activeSubCategories().get(i).getText());
+                    subCategories.add(activeSubCategories().get(j).getText().replaceAll("[^a-zA-ZÖö]", ""));
                 }
                 titleOfParentCategories().get(i).click();
             }
