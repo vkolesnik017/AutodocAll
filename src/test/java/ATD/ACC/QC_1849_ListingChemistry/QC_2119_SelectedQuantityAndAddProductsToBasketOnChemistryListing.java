18dc81981d4b484eda9db7eaa3a770c84ef0b400
Sergey <s.anisimov@autodoc.eu>
2020-10-19 17:40:48 +0300

    Create test QC-2446. Updated test QC-1384,QC-2119

--- src/test/java/ATD/ACC/QC_1849_ListingChemistry/QC_2119_SelectedQuantityAndAddProductsToBasketOnChemistryListing.java
+++ src/test/java/ATD/ACC/QC_1849_ListingChemistry/QC_2119_SelectedQuantityAndAddProductsToBasketOnChemistryListing.java
@@ -1,22 +1,27 @@
-package ATD.ACC.QC_2115_BlockWithProductsOnChemistryListing;
+package ATD.ACC.QC_1849_ListingChemistry;
 
+import ATD.Cart_page_Logic;
 import ATD.Listing_chemicals_Page_Logic;
 import Common.SetUp;
 import io.qameta.allure.Description;
 import io.qameta.allure.Flaky;
 import io.qameta.allure.Owner;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import java.sql.SQLException;
-
 import static ATD.CommonMethods.openPage;
 import static Common.SetUp.setUpBrowser;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
-public class QC_2119_SelectedQuantityProductsForBuyOnChemistryListing {
+public class QC_2119_SelectedQuantityAndAddProductsToBasketOnChemistryListing {
+
+    private String idProduct, valueCounter, idAddProduct, valueCounterAddProduct;
+    private Listing_chemicals_Page_Logic listing_chemicals_page_logic = new Listing_chemicals_Page_Logic();
+    private Cart_page_Logic cartPageLogic = new Cart_page_Logic();
+
 
     @BeforeClass
     void setUp() {
@@ -34,7 +39,15 @@ public class QC_2119_SelectedQuantityProductsForBuyOnChemistryListing {
     @Description(value = "Test Checks work quantity counter in card product on listing.")
     public void testCheckingWorkQuantityCounterInCardProduct(String route) {
         openPage(route);
-        new Listing_chemicals_Page_Logic().checkingWorkQuantityCounterOnDecreaseAndIncrease();
+        idProduct = listing_chemicals_page_logic.getIdProductListing();
+        listing_chemicals_page_logic.checkingWorkQuantityCounterOnDecreaseAndIncrease();
+        valueCounter = listing_chemicals_page_logic.getValueQuantityCounterFirstProductListing();
+        listing_chemicals_page_logic.clickBtnAddToBasketFirstProduct()
+                . cartClick();
+        idAddProduct = cartPageLogic.getIdAddedProduct();
+        valueCounterAddProduct = cartPageLogic.getValueQuantityCounter();
+        Assert.assertEquals(idProduct, idAddProduct);
+        Assert.assertEquals(valueCounter,valueCounterAddProduct);
     }
 
     @AfterMethod
