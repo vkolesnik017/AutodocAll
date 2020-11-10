5a77ee934bf28eab020fcf953a625fc7c5d4039b
Sergey <s.anisimov@autodoc.eu>
2020-11-10 09:17:15 +0200

    Create tests QC-2108, QC-2580 Alex test - QC-2720

--- src/test/java/ATD/Catalog/QC_2626_TecdocCatalog_SidebarCatalog_TopCategories/QC_2580_PresenceBlockWithEngineTypesAndTransitionByThem.java
+++ src/test/java/ATD/Catalog/QC_2626_TecdocCatalog_SidebarCatalog_TopCategories/QC_2580_PresenceBlockWithEngineTypesAndTransitionByThem.java
@@ -1,21 +1,25 @@
 package ATD.Catalog.QC_2626_TecdocCatalog_SidebarCatalog_TopCategories;
 
+import ATD.Moto_Catalog_model_page_Logic;
+import ATD.Moto_Catalog_page_Logic;
 import Common.SetUp;
 import io.qameta.allure.Description;
 import io.qameta.allure.Flaky;
 import io.qameta.allure.Owner;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import java.sql.SQLException;
-
 import static ATD.CommonMethods.openPage;
 import static Common.SetUp.setUpBrowser;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class QC_2580_PresenceBlockWithEngineTypesAndTransitionByThem {
+
+    private Moto_Catalog_model_page_Logic motoCatalogModelPageLogic = new Moto_Catalog_model_page_Logic();
+    private Moto_Catalog_page_Logic motoCatalogPageLogic = new Moto_Catalog_page_Logic();
 
     @BeforeClass
     void setUp() {
@@ -24,15 +28,24 @@ public class QC_2580_PresenceBlockWithEngineTypesAndTransitionByThem {
 
     @DataProvider(name = "routes", parallel = true)
     Object[] dataProvider() throws SQLException {
-        return new SetUp("ATD").setUpShopWithSubroutes("prod", "DE", "main",  "supplier");
+        return new SetUp("ATD").setUpShopWithSubroutes("subprod", "DE", "moto_main",  "moto_catalog_model2,moto_catalog_model4,moto_catalog_model5");
     }
 
     @Test(dataProvider = "routes")
     @Flaky
     @Owner(value = "Sergey_QA")
-    @Description(value = "Test checking display parens between brand page and AWS")
-    public void testCheckingDisplayParensBetweenBrandPageAndAws(String route) {
+    @Description(value = "Test checking presence block with engine types and transition by them only moto")
+    public void testCheckingPresenceBlockWithEngineTypesAndTransitionByThem(String route) {
         openPage(route);
+        motoCatalogModelPageLogic.checkPresenceModelAndEnginesTypeBlock()
+                .clickOnTooltipFromModelAndEnginesTypeBlock()
+                .checkConformityVehicleInTooltipFromModelAndEngineBlock()
+                .clickBtnPlusFromModelAndEnginesTypeBlock();
+        String dataEngine = motoCatalogModelPageLogic.getDataFromFirstPositionInModelAndEngineBlock();
+        motoCatalogModelPageLogic.clickFirstPositionFromModelAndEngineBlock();
+        motoCatalogPageLogic. openMotoSelectorsBlock();
+        String dataEngineFromSelector = motoCatalogPageLogic.getDataFromMotorOfMotoSelector();
+        Assert.assertEquals(dataEngine, dataEngineFromSelector);
 
     }
 
