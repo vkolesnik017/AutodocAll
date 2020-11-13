fb05023d042f83dd153a1b2bca90e68f878794d7
r.chelombitko <r.chelombitko@gmail.com>
2020-11-13 11:22:26 +0200

    Work in tests QC-2725, QC-2724

--- src/main/java/AWS/SearchOrders_page_aws.java
+++ src/main/java/AWS/SearchOrders_page_aws.java
@@ -1,8 +1,11 @@
 package AWS;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
+import static ATD.CommonMethods.openPage;
+import static Common.CommonMethods.getExpectedCalendarData;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -15,6 +18,48 @@ public class SearchOrders_page_aws {
         return $x("//a[@class='btn btn-primary control-add']");
     }
 
+    private SelenideElement inputDateFrom() {
+        return $x("//input[@name='Filter[fromDate]']");
+    }
+
+    private SelenideElement inputDataTo() {
+        return $x("//input[@name='Filter[toDate]']");
+    }
+
+    private SelenideElement searchBtn() {
+        return $x("//button[@name='search']");
+    }
+
+    private SelenideElement groupFieldSelector() {
+        return $x("//select[@name='Filter[groupList]']");
+    }
+
+    private ElementsCollection orderLine() {
+        return $$x("//td[@class='w-20 select-row']/..");
+    }
+
+
+
+    @Step("Click button search. SearchOrders_page_aws")
+    public SearchOrders_page_aws clickSearchBtn() {
+        searchBtn().click();
+        return this;
+    }
+
+    @Step("Fields inputs data from and data to. SearchOrders_page_aws")
+    public SearchOrders_page_aws fieldInputsDateFromAndDataTo(int minusMonths, int minusDays) {
+        String data = getExpectedCalendarData("yyyy-MM-dd", minusMonths, minusDays);
+        inputDateFrom().setValue(data);
+        inputDataTo().setValue(data);
+        return this;
+    }
+
+    @Step("Select expected Group field {expectedGroup}. SearchOrders_page_aws")
+    public SearchOrders_page_aws selectExpectedGroupField(String expectedGroup) {
+        groupFieldSelector().selectOptionContainingText(expectedGroup);
+        return this;
+    }
+
     @Step("Click button add order. SearchOrders_page_aws")
     public OrderAdd_page_aws clickAddOrderBtn() {
         addOrderBtn().shouldBe(visible);
@@ -24,8 +69,26 @@ public class SearchOrders_page_aws {
 
     @Step("Open search order page. SearchOrders_page_aws")
     public SearchOrders_page_aws openSearchOrderPageWithLogin() {
-        open(searchOrderPageURL);
+        openPage(searchOrderPageURL);
         new Login_aws().loginInAws();
         return page(SearchOrders_page_aws.class);
+    }
+
+    @Step("Find order with a generated invoice and with order amount equal to or less than ten. SearchOrders_page_aws")
+    public Order_aws findOrderWithGeneratedInvoice() {
+        if (orderLine().size() > 0) {
+            for (int i = 0; i < orderLine().size(); i++) {
+                if (!orderLine().get(i).$x(".//div[@data-hint='Test']").isDisplayed()){
+                    int sum = Integer.parseInt(orderLine().get(i).$x("./..//a[@class='printBillPopup']/../..//a[@class='order-grandtotal']").getText());
+                    if (sum <= 10) {
+                        String id = orderLine().get(i).$x("./..//a[@class='printBillPopup']/../..//a[@class='order_link']").getText();
+                        System.out.println(id);
+                        orderLine().get(i).$x("./..//a[@class='printBillPopup']/../..//a[@class='order_link']").click();
+                        break;
+                    }
+                }
+            }
+        }
+        return page(Order_aws.class);
     }
 }
