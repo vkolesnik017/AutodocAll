3f7ad323ff0807fc7964a897c6cc579f34a57c82
vkolesnik017 <vkolesnik017@gmail.com>
2020-12-29 17:56:35 +0200

    added QC_3199, QC_2811

--- src/test/java/ATD/Listings/QC_445_ListingTecDoc/QC_3199_DisplayOfLinkingBlockInTecDoc.java
+++ src/test/java/ATD/Listings/QC_445_ListingTecDoc/QC_3199_DisplayOfLinkingBlockInTecDoc.java
@@ -22,7 +22,7 @@ public class QC_3199_DisplayOfLinkingBlockInTecDoc {
 
     @BeforeClass
     void setUp() {
-        setUpBrowser(false, "chrome", "77.0");
+        setUpBrowser(false, "chrome", "77.0", false);
     }
 
     @DataProvider(name = "routes", parallel = true)
