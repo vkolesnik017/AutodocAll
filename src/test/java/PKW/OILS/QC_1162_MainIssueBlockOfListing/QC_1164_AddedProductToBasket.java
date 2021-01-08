da7091b36ea804b3ceebf4c0f6184c855c0d4482
vkolesnik017 <vkolesnik017@gmail.com>
2021-01-08 12:25:40 +0200

    fix QC_1164

--- src/test/java/PKW/OILS/QC_1162_MainIssueBlockOfListing/QC_1164_AddedProductToBasket.java
+++ src/test/java/PKW/OILS/QC_1162_MainIssueBlockOfListing/QC_1164_AddedProductToBasket.java
@@ -27,6 +27,7 @@ public class QC_1164_AddedProductToBasket {
     void setUp() {
         setUpBrowser(false, "chrome", "77.0", false);
     }
+
     @DataProvider(name = "routes", parallel = true)
     Object[] dataProvider() throws SQLException {
         return new Common.SetUp("PKW").setUpShopWithSubroutes("prod", "DE", "main", "motoroil_viscosity,motoroil_viscosity_brand,motoroil_specification,motoroil_release,motoroil_brand");
