8efea755a027de9c590b16f364b8011fffe659c1
olhalavrynenko <o.lavrinenko@autodoc.eu>
2021-01-19 18:01:05 +0200

    refactor test QC-2635 fix test QC-717

--- src/main/java/ATD/Widerruf_static_page_Logic.java
+++ src/main/java/ATD/Widerruf_static_page_Logic.java
@@ -2,17 +2,19 @@ package ATD;
 
 import io.qameta.allure.Step;
 
-import static ATD.CommonMethods.clickable;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.*;
 
 public class Widerruf_static_page_Logic extends Widerruf_static_page {
 
     @Step("Checks items. Widerruf_static_page")
-    public Widerruf_static_page_Logic checkItemsOnPage(){
+    public Widerruf_static_page_Logic checkItemsOnPage() {
         title().shouldBe(visible);
         mainBlock().shouldBe(visible);
-        emailLink1().shouldBe(clickable);
-        emailLink2().shouldBe(clickable);
+        for (int i = 0; i < emailLinks().size(); i++) {
+            emailLinks().get(i).shouldHave(attribute("href", "mailto:info@autodoc.de")).shouldHave(text("info@autodoc.de"));
+        }
+        emailLinks().shouldHave(size(2));
         return this;
     }
 }
