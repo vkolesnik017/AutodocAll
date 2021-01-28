4f1368cde9bcf31ecf0b9870e7a5950450ce2259
r.chelombitko <r.chelombitko@gmail.com>
2021-01-28 17:37:36 +0200

    work with test QC-3033

--- src/test/java/ATD/ForTest.java
+++ src/test/java/ATD/ForTest.java
@@ -7,9 +7,6 @@ import org.testng.annotations.Test;
 import java.sql.SQLException;
 import java.util.*;
 
-import static Common.CommonMethods.generationRandomDates;
-import static Common.CommonMethods.roundOfTheCost;
-
 
 public class ForTest extends SetUp {
 
@@ -17,17 +14,17 @@ public class ForTest extends SetUp {
     @Test()
     public void test() throws SQLException {
         List<String> test = new DataBase().getNameRequisitesMethod("bank_requisites_atd", "other", "Owner", "Bank", "IBAN", "Sort Code");
-        }
+    }
 
 
     @Test()
     public void test2() throws SQLException {
-        String [] payments = {"HypoVereinsbank", "PayPal", "Be2bill"};
+        String[] payments = {"HypoVereinsbank", "PayPal", "Be2bill"};
         int minValue = 0;
         int randomIndex = minValue + (int) (Math.random() * payments.length);
         String paymentName = payments[randomIndex];
     }
-    }
 
+}
 
 
