9929c098b6ce31abb947cc9af5de4b205e409dcd
olhalavrynenko <o.lavrinenko@autodoc.eu>
2021-01-28 17:51:28 +0200

    move set QC_2321 to General Common move set QC_1870, 1838, 1894 sets to QC-3031 delete StaticPage_PKW.xml refactor  Section ACC xml move QC-944 to QC- 3007

---
+++ src/test/java/PKW/Section_ACC/QC_1417_MainChemistry/QC_1613.java
@@ -0,0 +1,44 @@
+package PKW.Section_ACC.QC_1417_MainChemistry;
+
+import Common.SetUp;
+import PKW.Index_chemicals_page_Logic;
+import io.qameta.allure.Description;
+import io.qameta.allure.Flaky;
+import io.qameta.allure.Owner;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.sql.SQLException;
+
+import static Common.SetUp.setUpBrowser;
+import static PKW.CommonMethods.openPage;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class QC_1613 {
+
+    @BeforeClass
+    void setUp() {
+        setUpBrowser(false, "chrome", "77.0", false);
+    }
+
+    @DataProvider(name = "route", parallel = true)
+    Object[] dataProviderProducts() throws SQLException {
+        return new SetUp("PKW").setUpShopWithSubroutes("prod", "DE", "main", "index_chemicals");
+    }
+
+    @Test(dataProvider = "route")
+    @Flaky
+    @Owner(value = "Sergey-QA")
+    @Description(value = "Test checks opening product characteristics after hover on mini card in top products block.")
+    public void testCheckingHoverFirstProductInTopProductsBlock(String route) {
+        openPage(route);
+        new Index_chemicals_page_Logic().checkingHoverFirstProductInTopProductsBlock();
+    }
+
+    @AfterMethod
+    public void close() {
+        closeWebDriver();
+    }
+}
---
+++ src/test/java/PKW/Section_ACC/QC_3013_Accessories_Listing/QC_2293.java
@@ -0,0 +1,42 @@
+package PKW.Section_ACC.QC_3013_Accessories_Listing;
+
+import Common.SetUp;
+import PKW.Listing_accessories_page_Logic;
+import io.qameta.allure.Description;
+import io.qameta.allure.Flaky;
+import io.qameta.allure.Owner;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import java.sql.SQLException;
+import static Common.SetUp.setUpBrowser;
+import static PKW.CommonMethods.openPage;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class QC_2293 {
+
+    @BeforeClass
+    void setUp() {
+        setUpBrowser(false, "chrome", "77.0", false);
+    }
+
+    @DataProvider(name = "route", parallel = true)
+    Object[] dataProviderProducts() throws SQLException {
+        return new SetUp("PKW").setUpShopWithSubroutes("prod", "DE", "main", "listing_accessories");
+    }
+
+    @Test(dataProvider = "route")
+    @Flaky
+    @Owner(value = "Sergey-QA")
+    @Description(value = "Test checking work buttons in pagination.")
+    public void testCheckingWorkBtnInPagination(String route) {
+        openPage(route);
+        new Listing_accessories_page_Logic().checkingWorkBtnInPagination();
+    }
+
+    @AfterMethod
+    public void close() {
+        closeWebDriver();
+    }
+}
