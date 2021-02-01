4b674c71b60b12e4cf72a02d9cfb55d93a905fed
Sergey <s.anisimov@autodoc.eu>
2021-02-01 10:20:54 +0200

    Work by QC_2979

--- src/test/java/ATD/Basket/QC_2915_OptionalFieldFiscalCodeForRomaniaItalyPortugal/QC_2979.java
+++ src/test/java/ATD/Basket/QC_2915_OptionalFieldFiscalCodeForRomaniaItalyPortugal/QC_2979.java
@@ -1,7 +1,6 @@
 package ATD.Basket.QC_2915_OptionalFieldFiscalCodeForRomaniaItalyPortugal;
 
 import AWS.Order_aws;
-import Common.SetUp;
 import io.qameta.allure.Description;
 import io.qameta.allure.Flaky;
 import io.qameta.allure.Owner;
@@ -9,14 +8,16 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import java.sql.SQLException;
-import static ATD.CommonMethods.openPage;
 import static Common.SetUp.setUpBrowser;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class QC_2979 {
 
-    private String mail = "qc_2979_autotest@mailinator.com";
+    private String user = "qc_2979_autotest@mailinator.com" + "userId = 22703867";  //TODO  If you need a new order, then do it with this user
+
+    private String orderIdForIT = "42404636";
+    private String orderIdForPT = "42405232";
+    private String orderIdForRO = "42404963";
 
 
     @BeforeClass
@@ -26,10 +27,7 @@ public class QC_2979 {
 
     @DataProvider(name = "orderID", parallel = true)
     Object[] dataProvider() {
-        return new Object[][] {
-                {"42404636"},
-                {"42404963"},
-                {"42405232"}
+        return new Object[][] { {orderIdForIT}, {orderIdForPT}, {orderIdForRO}
         };
     }
 
@@ -38,7 +36,7 @@ public class QC_2979 {
     @Flaky
     @Owner(value = "Sergey_QA")
     @Description(value = "Successful saving of changes, with a filled / empty field \"Fiscal code\" (editing an order in AWS)")
-    public void testSuccessfulSavingOfChangesInFiscalCodeField(String orderID) throws SQLException {
+    public void testSuccessfulSavingOfChangesInFiscalCodeField(String orderID) {
         new Order_aws(orderID).openOrderInAwsWithLogin();
 
 
