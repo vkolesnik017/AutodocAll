ce4042b8f8367749d4ba1ff47059c09c1efca619
vkolesnik017 <vkolesnik017@gmail.com>
2021-02-01 13:51:00 +0200

    added QC_2797. update QC_254

--- src/main/java/ATD/Static_page_atd_page.java
+++ src/main/java/ATD/Static_page_atd_page.java
@@ -1,12 +1,59 @@
 package ATD;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$x;
 
 public class Static_page_atd_page {
 
-    SelenideElement helpingBlock() {return $(".atd-hilft");}
+    SelenideElement helpingBlock() {
+        return $(".atd-hilft");
+    }
 
-    SelenideElement pdfLinkInHelpingBLock() {return $(".hilft-article__files>span");}
+    SelenideElement pdfLinkInHelpingBLock() {
+        return $(".hilft-article__files>span");
+    }
+
+    SelenideElement headlineOfHelpingPressBlock() {
+        return $(".atd-hilft__header");
+    }
+
+    SelenideElement mainFormOfHelpingPressBlock() {
+        return $(".atd-hilft__main");
+    }
+
+    SelenideElement articleBlockOfMainFormOfHelpingPressBlock() {
+        return $(".atd-hilft__article");
+    }
+
+    SelenideElement covidImageOfMainFormOfHelpingPressBlock() {
+        return $(".atd-hilft__aside");
+    }
+
+    SelenideElement textOfArticleOfHelpingPressBlock() {
+        return $(".atd-hilft__text");
+    }
+
+    SelenideElement pdfBlockOfArticleOfHelpingPressBlock() {
+        return $(".hilft-article");
+    }
+
+    SelenideElement imageOfPdfBlockOfHelpingPressBlock() {
+        return $(".hilft-article__img");
+    }
+
+    SelenideElement contentOfPdfBlockOfHelpingPressBlock() {
+        return $(".hilft-article__content");
+    }
+
+    SelenideElement headlineOfContentPdfBlock() {
+        return $(".hilft-article__title");
+    }
+
+    ElementsCollection visiblePressLinks() {
+        return $$x("//div[@class='slick-track']/div").filter(visible);
+    }
 }
