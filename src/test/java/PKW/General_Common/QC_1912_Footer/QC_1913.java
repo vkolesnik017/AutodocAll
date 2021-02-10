5b472c7f9888d8b8c3a046f60bcea8f937bce791
Sergey <s.anisimov@autodoc.eu>
2021-02-10 12:50:26 +0200

    Started - QC_1913

--- src/test/java/PKW/General_Common/QC_1912_Footer/QC_1913.java
+++ src/test/java/PKW/General_Common/QC_1912_Footer/QC_1913.java
@@ -5,17 +5,21 @@ import PKW.Main_page_Logic;
 import io.qameta.allure.Description;
 import io.qameta.allure.Flaky;
 import io.qameta.allure.Owner;
+import mailinator.WebMail;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static Common.SetUp.setUpBrowser;
-import static PKW.CommonMethods.mailRandomMailinator;
 import static PKW.CommonMethods.openPage;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class QC_1913 {
+
+
+    private Main_page_Logic mainPageLogic = new Main_page_Logic();
+
 
     @BeforeClass
     void setUp() {
@@ -29,12 +33,21 @@ public class QC_1913 {
 
     @Test(dataProvider = "route")
     @Flaky
-    @Owner(value = "LavrynenkoOlha")
+    @Owner(value = "Sergey_QA")
     @Description(value = "Positive Sending The Subscribe Form in the Footer")
     public void testSendingTheSubscribeForm(String route) {
         openPage(route);
-        String mail = mailRandomMailinator("1913");
-        new Main_page_Logic().positiveSendingSubscribeForm(mail);
+        String mail = mainPageLogic.openRegistrationFormInHeader()
+                .fillingRegistrationFields("qc_1913_");
+        mainPageLogic.clickBtnToPRFromPopupAuthorizationSuccessfully()
+                .checkingPresenceIdUser();
+        mainPageLogic.positiveSendingSubscribeForm(mail);
+        new WebMail().openMail(mail)
+                .checkLetterInfoText(1, "just now", "Noch ein weiterer Schritt und Sie haben unseren Newsletter abonniert.")
+                .openLetter(1);
+
+
+
     }
 
     @AfterMethod
