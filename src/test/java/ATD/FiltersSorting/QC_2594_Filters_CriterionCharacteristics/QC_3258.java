0e1bb583432d9fb17ebca028cc7c50634899a116
vkolesnik017 <vkolesnik017@gmail.com>
2021-02-19 18:16:33 +0200

    finished QC_3253, started QC_3258

---
+++ src/test/java/ATD/FiltersSorting/QC_2594_Filters_CriterionCharacteristics/QC_3258.java
@@ -0,0 +1,56 @@
+package ATD.FiltersSorting.QC_2594_Filters_CriterionCharacteristics;
+
+import ATD.Motoroil_brand_page_Logic;
+import Common.SetUp;
+import io.qameta.allure.Description;
+import io.qameta.allure.Flaky;
+import io.qameta.allure.Owner;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.sql.SQLException;
+
+import static ATD.CommonMethods.openPage;
+import static Common.CommonMethods.checkingContainsUrl;
+import static Common.SetUp.setUpBrowser;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class QC_3258 {
+
+    private String firstIdValue = "1L";
+    private String secondIdValue = "2L";
+    Motoroil_brand_page_Logic motoroilPage = new Motoroil_brand_page_Logic();
+
+    @BeforeClass
+    void setUp() {
+        setUpBrowser(false, "chrome", "77.0", false);
+    }
+
+    @DataProvider(name = "route", parallel = true)
+    Object[] dataProvider() throws SQLException {
+        return new SetUp("ATD").setUpShopWithSubroutes("prod", "DE", "main", "motoroil_brand,motoroil_viscosity2,motoroil_search");
+    }
+
+    @Test(dataProvider = "route")
+    @Flaky
+    @Owner(value = "Kolesnik")
+    @Description(value = "Test checks Application of the Inhalt filter on oil listings")
+    public void testCheckApplicationInhaltFilter(String route) {
+        openPage(route);
+
+        motoroilPage
+                .presenceInhaltFilterInSidebar()
+                .setInhaltFilterInSidebar(firstIdValue)
+                .appearsOfLoader()
+                .checkSelectedCheckBoxInhaltFilter(firstIdValue)
+                .checkDisplaySelectedInhaltFilterInProducts(firstIdValue);
+        checkingContainsUrl(firstIdValue.replaceAll("[^0-9]", ""));
+    }
+
+    @AfterMethod
+    private void close() {
+        closeWebDriver();
+    }
+}
