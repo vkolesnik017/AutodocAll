f7ad6c4751a29caa73959c389e32ad39b90ff6ef
r.chelombitko <r.chelombitko@gmail.com>
2021-02-25 17:10:12 +0200

    Update test QC-180 Checked and enabled the test QC-963 Finish tests QC-2728, QC-2730, QC-2733

---
+++ src/test/java/ATD/Basket/QC_1873_SafeOrder_ATD/QC_2730.java
@@ -0,0 +1,161 @@
+package ATD.Basket.QC_1873_SafeOrder_ATD;
+
+import ATD.Product_page_Logic;
+import ATD.Versand_static_page_Logic;
+import AWS.Order_aws;
+import Common.DataBase;
+import Common.SetUp;
+import io.qameta.allure.Description;
+import io.qameta.allure.Flaky;
+import io.qameta.allure.Owner;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.sql.SQLException;
+
+import static ATD.CommonMethods.*;
+import static Common.SetUp.setUpBrowser;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class QC_2730 {
+
+    private DataBase bd = new DataBase("ATD");
+    private Product_page_Logic product_page_logic = new Product_page_Logic();
+
+    @BeforeClass
+    void setUp() {
+        setUpBrowser(false, "chrome", "77.0", false);
+    }
+
+    @DataProvider(name = "shop", parallel = true)
+    Object[] dataProvider() throws SQLException {
+        return new SetUp("ATD").setUpShopsWithSubroute("prod", "EE,LV,LT,SI,EN", "main", "staticVersand");
+    }
+
+    @Test(dataProvider = "shop", priority = 1)
+    @Flaky
+    @Owner(value = "Chelombitko")
+    @Description(value = "Test checking for no free SO with ATD + MVP2 BASIK package")
+    public void testCheckingForNoFreeSO_withATD_Basik(String route) throws SQLException {
+        openPage(route);
+        String mail = "qc_2730_autotestbasik@mailinator.com";
+        String shop = getCurrentShopFromJSVarInHTML();
+        String soPrise = new Versand_static_page_Logic().getSafeOrderPrice();
+        openPage(bd.getFullRouteByRouteAndSubroute("prod", shop, "main", "product32"));
+        String orderID = product_page_logic.addProductToCart()
+                .closePopupOtherCategoryIfYes()
+                .cartClick()
+                .nextButtonClick()
+                .signIn(mail, password)
+                .chooseDeliveryCountryForShipping(shop)
+                .fillFieldTelNumForShipping("200+002")
+                .nextBtnClick()
+                .clickOnTheDesiredPaymentMethod(shop, "Bank")
+                .nextBtnClick()
+                .checkThatSafeOrderCheckboxIsSelected()
+                .checkSumSO(soPrise)
+                .nextBtnClick()
+                .getOrderNumber();
+        new Order_aws(orderID).openOrderInAwsWithLogin()
+                .checkSafeOrderPrice(soPrise)
+                .reSaveOrder()
+                .checkCurrentStatusInOrder("Testbestellungen");
+    }
+
+    @Test(dataProvider = "shop", priority = 2)
+    @Flaky
+    @Owner(value = "Chelombitko")
+    @Description(value = "Test checking for no free SO with ATD + MVP2 OPTIMUM package")
+    public void testCheckingForNoFreeSO_withATD_OPTIMUM(String route) throws SQLException {
+        openPage(route);
+        String mail = "qc_2730_autotestoptimum@mailinator.com";
+        String shop = getCurrentShopFromJSVarInHTML();
+        String soPrise = new Versand_static_page_Logic().getSafeOrderPrice();
+        openPage(bd.getFullRouteByRouteAndSubroute("prod", shop, "main", "product32"));
+        String orderID = product_page_logic.addProductToCart()
+                .closePopupOtherCategoryIfYes()
+                .cartClick()
+                .nextButtonClick()
+                .signIn(mail, password)
+                .chooseDeliveryCountryForShipping(shop)
+                .fillFieldTelNumForShipping("200+002")
+                .nextBtnClick()
+                .clickOnTheDesiredPaymentMethod(shop, "Bank")
+                .nextBtnClick()
+                .checkThatSafeOrderCheckboxIsSelected()
+                .checkSumSO(soPrise)
+                .nextBtnClick()
+                .getOrderNumber();
+        new Order_aws(orderID).openOrderInAwsWithLogin()
+                .checkSafeOrderPrice(soPrise)
+                .reSaveOrder()
+                .checkCurrentStatusInOrder("Testbestellungen");
+    }
+
+    @Test(dataProvider = "shop", priority = 3)
+    @Flaky
+    @Owner(value = "Chelombitko")
+    @Description(value = "Test checking for no free SO with ATD + MVP2 TRIAL package")
+    public void testCheckingForNoFreeSO_withATD_TRIAL(String route) throws SQLException {
+        openPage(route);
+        String mail = "qc_2730_autotesttrial@mailinator.com";
+        String shop = getCurrentShopFromJSVarInHTML();
+        String soPrise = new Versand_static_page_Logic().getSafeOrderPrice();
+        openPage(bd.getFullRouteByRouteAndSubroute("prod", shop, "main", "product32"));
+        String orderID = product_page_logic.addProductToCart()
+                .closePopupOtherCategoryIfYes()
+                .cartClick()
+                .nextButtonClick()
+                .signIn(mail, password)
+                .chooseDeliveryCountryForShipping(shop)
+                .fillFieldTelNumForShipping("200+002")
+                .nextBtnClick()
+                .clickOnTheDesiredPaymentMethod(shop, "Bank")
+                .nextBtnClick()
+                .checkThatSafeOrderCheckboxIsSelected()
+                .checkSumSO(soPrise)
+                .nextBtnClick()
+                .getOrderNumber();
+        new Order_aws(orderID).openOrderInAwsWithLogin()
+                .checkSafeOrderPrice(soPrise)
+                .reSaveOrder()
+                .checkCurrentStatusInOrder("Testbestellungen");
+    }
+
+    @Test(dataProvider = "shop", priority = 4)
+    @Flaky
+    @Owner(value = "Chelombitko")
+    @Description(value = "Test checking for no free SO with ATD + MVP2 PRO package")
+    public void testCheckingForNoFreeSO_withATD_PRO(String route) throws SQLException {
+        openPage(route);
+        String mail = "qc_2730_autotestpro@mailinator.com";
+        String shop = getCurrentShopFromJSVarInHTML();
+        String soPrise = new Versand_static_page_Logic().getSafeOrderPrice();
+        openPage(bd.getFullRouteByRouteAndSubroute("prod", shop, "main", "product32"));
+        String orderID = product_page_logic.addProductToCart()
+                .closePopupOtherCategoryIfYes()
+                .cartClick()
+                .nextButtonClick()
+                .signIn(mail, password)
+                .chooseDeliveryCountryForShipping(shop)
+                .fillFieldTelNumForShipping("200+002")
+                .nextBtnClick()
+                .clickOnTheDesiredPaymentMethod(shop, "Bank")
+                .nextBtnClick()
+                .checkThatSafeOrderCheckboxIsSelected()
+                .checkSumSO(soPrise)
+                .nextBtnClick()
+                .getOrderNumber();
+        new Order_aws(orderID).openOrderInAwsWithLogin()
+                .checkSafeOrderPrice(soPrise)
+                .reSaveOrder()
+                .checkCurrentStatusInOrder("Testbestellungen");
+    }
+
+    @AfterMethod
+    private void close() {
+        closeWebDriver();
+    }
+}
