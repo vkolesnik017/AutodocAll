41dacde8c6472fe589ddfd417b6d1f848b9c70e1
vkolesnik017 <vkolesnik017@gmail.com>
2021-02-26 00:39:00 +0200

    changed name of  Accessories_listing_criteria_page in step description

--- src/main/java/ATD/Accessories_listing_criteria_page_Logic.java
+++ src/main/java/ATD/Accessories_listing_criteria_page_Logic.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Condition.visible;
 public class Accessories_listing_criteria_page_Logic extends Accessories_listing_criteria_page {
 
 
-    @Step("check Headline of generics block . Listing_instruments_page")
+    @Step("check Headline of generics block . Accessories_listing_criteria_page")
     public Accessories_listing_criteria_page_Logic checkHeadlineOfGenericsBlock(String headline) {
         headlineOfGenericsBlock().shouldBe(visible).shouldHave(text(headline));
         return this;
