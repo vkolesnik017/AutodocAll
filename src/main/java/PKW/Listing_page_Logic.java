374c56f1db5518f9727093ce53e43e2491e2a1cd
Evgeniy <e.chebanenko@autodoc.eu>
2021-03-30 23:03:42 +0300

    create test: QC_2974, QC_475, QC_467, QC_468

--- src/main/java/PKW/Listing_page_Logic.java
+++ src/main/java/PKW/Listing_page_Logic.java
@@ -1,16 +1,19 @@
 package PKW;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
-import org.testng.Assert;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import static Common.CommonMethods.checkingContainsUrl;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Condition.or;
 import static com.codeborne.selenide.Selenide.back;
 import static com.codeborne.selenide.Selenide.page;
+import static org.testng.Assert.assertEquals;
 
 
 public class Listing_page_Logic extends Listing_page {
@@ -67,7 +70,7 @@ public class Listing_page_Logic extends Listing_page {
         for (int i = 0; i < titleOfProductInListing().size(); i++) {
             String name = titleOfProductInListing().get(i).getText();
             String actualName = name.replace(name.substring(name.indexOf(" ")), "");
-            Assert.assertEquals(brandName, actualName);
+            assertEquals(brandName, actualName);
         }
         return this;
     }
@@ -81,7 +84,7 @@ public class Listing_page_Logic extends Listing_page {
                     moreBtnInProduct().click();
                 }
                 String characteristicName = nameProductCharacteristicINParametersBlock(nameCharacteristic).getText();
-                Assert.assertEquals(expectedCharacteristicName, characteristicName);
+                assertEquals(expectedCharacteristicName, characteristicName);
             }
         }
         return this;
@@ -103,7 +106,7 @@ public class Listing_page_Logic extends Listing_page {
                 searchPageTitle().scrollTo();
                 clickProductOnListing();
                 String characteristicName = new Product_page().nameProductCharacteristic(nameCharacteristic).getText();
-                Assert.assertEquals(expectedCharacteristicName, characteristicName);
+                assertEquals(expectedCharacteristicName, characteristicName);
                 back();
             }
         }
@@ -122,8 +125,8 @@ public class Listing_page_Logic extends Listing_page {
             titleOfProductIOnListing().get(i).shouldNotHave(text(brand));
             priceOfAnotherBrands.add(Double.parseDouble(priceOfProduct().get(i).getText().replaceAll("[^0-9,]", "").replace(",", ".")));
         }
-        Assert.assertEquals(priceRidex, getExpectedSortedPrices(priceRidex));
-        Assert.assertEquals(priceOfAnotherBrands, getExpectedSortedPrices(priceOfAnotherBrands));
+        assertEquals(priceRidex, getExpectedSortedPrices(priceRidex));
+        assertEquals(priceOfAnotherBrands, getExpectedSortedPrices(priceOfAnotherBrands));
         return this;
     }
 
@@ -139,5 +142,60 @@ public class Listing_page_Logic extends Listing_page {
         listProductBlock().shouldBe(visible);
         int countOfActiveExactBrand = btnAddToBasketWithBrand(brand).size();
         return countOfActiveExactBrand;
+    }
+
+    @Step("Checks elements on listing (Side filters block, Brand filter block, Pagination blocks). Listing_page")
+    public Listing_page_Logic checksElementsOnListing() {
+        blockOfBySideFilters().shouldBe(visible);
+        blockOfBySideFiltersSecond().shouldBe(visible);
+        blockOfBrandFilters().shouldBe(visible);
+        blockOfPagination().shouldBe(visible);
+        return this;
+    }
+
+    @Step("Method saves the order of products on listing, switches to table view and verifies that order of products remains the same. Listing_page")
+    public Listing_page_Logic compareProductsOrderBetweenListModeAndTableMode(int sizeProduct) {
+        ElementsCollection elementsWithArticles = productArticlesInListing().first(sizeProduct);
+        ArrayList<String> articlesInListMode = new ArrayList<>();
+        for (SelenideElement nameProduct : elementsWithArticles) {
+            String name = nameProduct.text().split(": ")[1];
+            articlesInListMode.add(name);
+        }
+        showListingInTableModeButton().click();
+        checkingContainsUrl("?list=table");
+        ElementsCollection articlesOnTableMode = articleProductsInTableMode().shouldHave(sizeGreaterThanOrEqual(sizeProduct));
+        for (int numberProductName = 0; numberProductName < articlesInListMode.size(); numberProductName++) {
+            String articleListMode = articlesInListMode.get(numberProductName);
+            String articleNumberTableMode = articlesOnTableMode.get(numberProductName).text().split(": ")[0];
+            assertEquals(articleListMode, articleNumberTableMode, "Product order " + articleListMode + " does not match between list mode and table mode");
+        }
+        return this;
+    }
+
+    @Step("Checks elements on search listing (Side filters block, Brand filter block, Pagination blocks). Listing_page")
+    public Listing_page_Logic checksElementsOnSearchListing() {
+        blockOfBySideFiltersSearchListing().shouldBe(visible);
+        blockOfBrandFiltersSearchListing().shouldBe(visible);
+        blockOfPagination().shouldBe(visible);
+        return this;
+    }
+
+    @Step("Method saves the order of products on table, switches to grid view and verifies that order of products remains the same. Listing_page")
+    public Listing_page_Logic compareProductsOrderBetweenTableModeAndGridMode(int sizeProduct) {
+        ElementsCollection elementsWithArticlesTable = articleProductsInTableMode().first(sizeProduct);
+        ArrayList<String> articlesOnTableMode = new ArrayList<>();
+        for (SelenideElement nameProduct : elementsWithArticlesTable) {
+            String name = nameProduct.text().split(": ")[0];
+            articlesOnTableMode.add(name);
+        }
+        showListingInGridModeButton().click();
+        checkingContainsUrl("?list=grid");
+        ElementsCollection articlesOnGridMode = articleProductsInGridMode().shouldHave(sizeGreaterThanOrEqual(sizeProduct));
+        for (int numberProductName = 0; numberProductName < articlesOnTableMode.size(); numberProductName++) {
+            String articleTableMode = articlesOnTableMode.get(numberProductName);
+            String articleNumberGridMode = articlesOnGridMode.get(numberProductName).text().split(": ")[1];
+            assertEquals(articleTableMode, articleNumberGridMode, "Product order " + articleTableMode + " does not match between table mode and grid mode");
+        }
+        return this;
     }
 }
